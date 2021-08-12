a1ba8ef4f6db875d6a7ef6745f9ac6db8a069738
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2021-08-12 20:18:47 +0000

    feat: release 0.2.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='ypricemagic',
     packages=find_packages(),
-    version='0.2.5',
+    version='0.2.6',
     description='Use this tool to extract historical on-chain price data from an archive node. Shoutout to @bantg and @nymmrx for their awesome work on yearn-exporter that made this library possible.',
     author='BobTheBuidler',
     author_email='bobthebuidlerdefi@gmail.com',
@@ -11,7 +11,7 @@ setup(
     license='MIT',
     install_requires=[
         'cachetools>=4.1.1',
-        'eth-brownie>=1.14.6',
+        'eth-brownie>=1.16.0',
         'joblib>=1.0.1'
     ],
     )
