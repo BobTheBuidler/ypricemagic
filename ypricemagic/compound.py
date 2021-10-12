0780867f5805b18449de24e0a248c38e715f1580
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2021-10-12 22:37:20 +0000

    fix: easytroller proxy implementation

--- ypricemagic/compound.py
+++ ypricemagic/compound.py
@@ -25,6 +25,7 @@ def get_markets():
         return dict(zip(names, results))
     elif chain.id == 137: # poly mainnet
         easytroller = Contract("0xcb3fA413B23b12E402Cfcd8FA120f983FB70d8E8")
+        easytroller = Contract.from_abi('Unitroller',"0xcb3fA413B23b12E402Cfcd8FA120f983FB70d8E8", Contract(easytroller.comptrollerImplementation()).abi)
         results = [easytroller.getAllMarkets()]
         names = ['easyfi']
         return dict(zip(names, results))
