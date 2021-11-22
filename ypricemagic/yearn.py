178fb84f9004f4231b502696ba47fb756620b035
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2021-11-22 07:18:40 +0000

    feat: multicall on all chains for yearn-like

--- ypricemagic/yearn.py
+++ ypricemagic/yearn.py
@@ -13,63 +13,76 @@ def get_price(token, block=None):
     # v1 vaults use getPricePerFullShare scaled to 18 decimals
     # v2 vaults use pricePerShare scaled to underlying token decimals
     vault = Contract(token)
-    if chain.id == 1:
-        if hasattr(vault, 'pricePerShare'):
-            share_price, underlying, decimals = fetch_multicall(
-                [vault, 'pricePerShare'],
-                [vault, 'token'],
-                [vault, 'decimals'],
-                block=block
-            )
-        if hasattr(vault, 'getPricePerFullShare') and hasattr(vault, 'token'):
-            share_price, underlying = fetch_multicall(
-                [vault, 'getPricePerFullShare'],
-                [vault, 'token'],
-                block=block
-            )
-            decimals = 18
-        if hasattr(vault, 'getPricePerFullShare') and hasattr(vault, 'underlying'):
-            share_price, underlying, decimals = fetch_multicall(
-                [vault, 'getPricePerFullShare'],
-                [vault, 'underlying'],
-                [vault, 'decimals'],
-                block=block
-            )
-        if hasattr(vault, 'getPricePerFullShare') and hasattr(vault, 'native'):
-            share_price, underlying, decimals = fetch_multicall(
-                [vault, 'getPricePerFullShare'],
-                [vault, 'native'],
-                [vault, 'decimals'],
-                block=block
-            )
-        if hasattr(vault, 'getPricePerShare'):
-            share_price, underlying, decimals = fetch_multicall(
-                [vault, 'getPricePerShare'],
-                [vault, 'token'],
-                [vault, 'decimals'],
-                block=block
-            )
+    #if chain.id == 1:
+    if hasattr(vault, 'pricePerShare'):
+        share_price, underlying, decimals = fetch_multicall(
+            [vault, 'pricePerShare'],
+            [vault, 'token'],
+            [vault, 'decimals'],
+            block=block
+        )
+    elif hasattr(vault, 'getPricePerFullShare') and hasattr(vault, 'token'):
+        share_price, underlying = fetch_multicall(
+            [vault, 'getPricePerFullShare'],
+            [vault, 'token'],
+            block=block
+        )
+        decimals = 18
+    elif hasattr(vault, 'getPricePerFullShare') and hasattr(vault, 'underlying'):
+        share_price, underlying, decimals = fetch_multicall(
+            [vault, 'getPricePerFullShare'],
+            [vault, 'underlying'],
+            [vault, 'decimals'],
+            block=block
+        )
+    elif hasattr(vault, 'getPricePerFullShare') and hasattr(vault, 'native'):
+        share_price, underlying, decimals = fetch_multicall(
+            [vault, 'getPricePerFullShare'],
+            [vault, 'native'],
+            [vault, 'decimals'],
+            block=block
+        )
+    elif vault.__dict__['_build']['contractName'] == 'BeefyVaultV6Matic':
+        share_price, underlying, decimals = fetch_multicall(
+            [vault, 'getPricePerFullShare'],
+            [vault, 'wmatic'],
+            [vault, 'decimals'],
+            block=block
+        )
+    elif hasattr(vault, 'getPricePerShare'):
+        share_price, underlying, decimals = fetch_multicall(
+            [vault, 'getPricePerShare'],
+            [vault, 'token'],
+            [vault, 'decimals'],
+            block=block
+        )
+    ''' might need this later for goofy L1s w/o multicall2        
     else:
         if hasattr(vault, 'pricePerShare'):
             share_price = vault.pricePerShare(block_identifier = block)
             underlying = vault.token(block_identifier = block)
             decimals = vault.decimals(block_identifier = block)
-        if hasattr(vault, 'getPricePerFullShare') and hasattr(vault, 'token'):
+        elif hasattr(vault, 'getPricePerFullShare') and hasattr(vault, 'token'):
             share_price = vault.getPricePerFullShare(block_identifier = block)
             underlying = vault.token(block_identifier = block)
             decimals = 18
-        if hasattr(vault, 'getPricePerFullShare') and hasattr(vault, 'underlying'):
+        elif hasattr(vault, 'getPricePerFullShare') and hasattr(vault, 'underlying'):
             share_price = vault.getPricePerFullShare(block_identifier = block)
             underlying = vault.underlying(block_identifier = block)
             decimals = vault.decimals(block_identifier = block)
-        if hasattr(vault, 'getPricePerFullShare') and hasattr(vault, 'want'):
+        elif hasattr(vault, 'getPricePerFullShare') and hasattr(vault, 'want'):
             share_price = vault.getPricePerFullShare(block_identifier = block)
             underlying = vault.want(block_identifier = block)
             decimals = vault.decimals(block_identifier = block)
-        if hasattr(vault, 'getPricePerShare'):
+        elif hasattr(vault, 'getPricePerFullShare') and hasattr(vault, 'native'):
+            share_price = vault.getPricePerFullShare(block_identifier=block)
+            underlying = vault.native(block_identifier=block)
+            decimals = vault.decimals(block_identifier=block)
+        elif hasattr(vault, 'getPricePerShare'):
             share_price = vault.getPricePerShare(block_identifier = block)
             underlying = vault.token(block_identifier = block)
             decimals = vault.decimals(block_identifier = block)
+            '''
 
     try:
         return [share_price / 10 ** decimals, underlying]
