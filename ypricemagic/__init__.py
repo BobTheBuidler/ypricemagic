8b9d6a89f821ff62b389a790cd1338c65c6461cf
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-01-11 09:46:42 +0000

    feat: shortcuts to useful things

--- ypricemagic/__init__.py
+++ ypricemagic/__init__.py
@@ -1,5 +1,42 @@
-from brownie import network
 import os
+
+from brownie import network
+
+from ypricemagic.constants import dai, usdc, wbtc, weth
+from ypricemagic.networks import Network
+from ypricemagic.utils.contracts import Contract
+from ypricemagic.utils.multicall import fetch_multicall
+from ypricemagic.utils.raw_calls import (_balanceOf, _balanceOfReadable,
+                                         _decimals, _symbol, _totalSupply,
+                                         _totalSupplyReadable)
+
+__all__ = [
+    ### you can reach the below functions, classes, and variables using ###
+    ###  `ypricemagic.__name__` or `from ypricemagic import __name__`.  ###
+
+    # contract stuff
+    'Contract',
+
+    # network stuff
+    'Network',
+
+    # constants
+    'weth',
+    'dai',
+    'usdc',
+    'wbtc',
+
+    # multicall
+    'fetch_multicall',
+
+    # raw calls
+    '_decimals',
+    '_symbol',
+    '_balanceOf',
+    '_balanceOfReadable',
+    '_totalSupply',
+    '_totalSupplyReadable',
+]
 
 if not network.is_connected():
     try:
@@ -7,4 +44,5 @@ if not network.is_connected():
     except KeyError:
         raise KeyError('In order to use pricemagic outside of a brownie project directory, you will need to set $BROWNIE_NETWORK_ID environment variable with the id of your preferred brownie network connection.')
     network.connect(network_name)
+
 
