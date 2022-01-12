d9722c4e385029f97d51657bf67c48a840cc9f2c
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-01-12 02:29:43 +0000

    fix: raw_calls

--- ypricemagic/utils/raw_calls.py
+++ ypricemagic/utils/raw_calls.py
@@ -3,11 +3,12 @@ from brownie import convert, web3
 from eth_utils import encode_hex
 from eth_utils import function_signature_to_4byte_selector as fourbyte
 from toolz.itertoolz import concat
+from ypricemagic import Contract
 
 
 def _decimals(contract_address, block=None):
-    data = raw_call(contract_address, "decimals()", block=block)
-    return convert.to_int(data)
+    try: return convert.to_int(raw_call(contract_address, "decimals()", block=block))
+    except OverflowError: return Contract(contract_address).decimals(block_identifier=block)
 
 
 def _symbol(contract_address, block=None):
@@ -16,8 +17,8 @@ def _symbol(contract_address, block=None):
 
 
 def _totalSupply(contract_address, block=None):
-    data = raw_call(contract_address, "totalSupply()", block=block)
-    return convert.to_int(data)
+    try: return convert.to_int(raw_call(contract_address, "totalSupply()", block=block))
+    except OverflowError: return Contract(contract_address).totalSupply(block_identifier=block)
 
 
 def _totalSupplyReadable(contract_address, block=None):
@@ -25,7 +26,7 @@ def _totalSupplyReadable(contract_address, block=None):
 
 
 def _balanceOf(call_address, input_address, block=None):
-    data = raw_call(call_address, "balanceOf(address)", block=block, input=input_address)
+    data = raw_call(call_address, "balanceOf(address)", block=block, inputs=input_address)
     return convert.to_int(data)
 
 
@@ -33,13 +34,16 @@ def _balanceOfReadable(call_address, input_address, block=None):
     return _balanceOf(call_address, input_address, block=block) / 10 ** _decimals(call_address, block)
 
 
-def raw_call(contract_address: str, method: str, block=None, inputs=None):
+def raw_call(contract_address: str, method: str, block=None, inputs=None, output:str=None):
     '''
     call a contract with only address and method.
     only works with 1 input, ie `balanceOf(address)`
     '''
     data = {'to': convert.to_address(contract_address),'data': prepare_data(method,inputs)}
-    return web3.eth.call(data,block_identifier=block)
+    response = web3.eth.call(data,block_identifier=block)
+    if output is None: return response
+    elif output == 'address': return convert.to_address(f'0x{response.hex()[-40:]}')
+    elif output in ['int','uint','uint256']: return convert.to_int(response)
 
 def prepare_data(method, inputs=None):
     method = fourbyte(method)
@@ -55,6 +59,6 @@ def prepare_data(method, inputs=None):
         raise ValueError(f'Unsupported input type. Supported types are: str, int, bytes, Set[str,int,bytes], List[str,int,bytes], Tuple[str,int,bytes], Dict[Any:[str,int,bytes]]')
 
 def prepare_input(input):
-    try: return '000000000000000000000000' + convert.to_address(input)
+    try: return '000000000000000000000000' + convert.to_address(input)[2:]
     except: raise 
     
