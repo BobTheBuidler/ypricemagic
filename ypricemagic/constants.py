e74ce2f547b213b1014aed9cda2f722e7077e71f
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-01-12 02:30:36 +0000

    chore: refactor constants

--- ypricemagic/constants.py
+++ ypricemagic/constants.py
@@ -1,6 +1,7 @@
 from brownie import chain
 
 from ypricemagic.utils.contracts import Contract
+from ypricemagic.networks import Network
 
 sushi = None
 
@@ -12,7 +13,36 @@ if chain.id == 1:
     usdt = Contract("0xdAC17F958D2ee523a2206206994597C13D831ec7")
     sushi = Contract("0x6B3595068778DD592e39A122f4f5a5cF09C90fE2")
 
-    STABLECOINS = {
+elif chain.id == 56:
+    weth = Contract("0x8d0e18c97e5dd8ee2b539ae8cd3a3654df5d79e5")
+    usdc = Contract("0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d")
+    dai = Contract("0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3")
+    wbtc = Contract("0x2ccb7c8c51e55c2364b555ff6e6e3f7246499e16")
+    usdt = Contract("0x55d398326f99059ff775485246999027b3197955")
+    wbnb = Contract("0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c")
+    cake = Contract("0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82")
+
+elif chain.id == 137: #poly
+    weth = Contract("0x7ceb23fd6bc0add59e62ac25578270cff1b9f619")
+    usdc = Contract("0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174")
+    dai = Contract("0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063")
+    wbtc = Contract("0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6")
+    usdt = Contract("0xc2132D05D31c914a87C6611C10748AEb04B58e8F")
+    wmatic = Contract("0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270")
+    
+elif chain.id == 250:
+    weth = Contract('0x74b23882a30290451A17c44f4F05243b6b58C76d')
+    usdc = Contract('0x04068DA6C83AFCFA0e13ba15A6696662335D5B75')
+    dai = Contract('0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E')
+    wbtc = Contract('0x321162Cd933E2Be498Cd2267a90534A804051b11')
+    usdt = Contract('0x049d68029688eAbF473097a2fC38ef61633A3C7A') #fusdt
+    wftm = Contract('0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83')
+
+elif chain.id == 42161: pass
+elif chain.id == 43114: pass
+
+STABLECOINS = {
+    Network.Mainnet: {
         "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48": "usdc",
         "0x0000000000085d4780B73119b644AE5ecd22b376": "tusd",
         "0x6B175474E89094C44Da98b954EedeAC495271d0F": "dai",
@@ -27,18 +57,8 @@ if chain.id == 1:
         "0xBC6DA0FE9aD5f3b0d58160288917AA56653660E9": "alusd",
         "0x8e870d67f660d95d5be530380d0ec0bd388289e1": "pax",
         "0xe2f2a5C287993345a840Db3B0845fbC70f5935a5": "musd"
-    }
-
-elif chain.id == 56:
-    weth = Contract("0x8d0e18c97e5dd8ee2b539ae8cd3a3654df5d79e5")
-    usdc = Contract("0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d")
-    dai = Contract("0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3")
-    wbtc = Contract("0x2ccb7c8c51e55c2364b555ff6e6e3f7246499e16")
-    usdt = Contract("0x55d398326f99059ff775485246999027b3197955")
-    wbnb = Contract("0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c")
-    cake = Contract("0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82")
-
-    STABLECOINS = {
+    },
+    Network.BinanceSmartChain: {
         "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d": "usdc",
         "0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3": "dai",
         "0x55d398326f99059ff775485246999027b3197955": "usdt",
@@ -46,39 +66,35 @@ elif chain.id == 56:
         "0x23396cF899Ca06c4472205fC903bDB4de249D6fC": "wust",
         "0x14016E85a25aeb13065688cAFB43044C2ef86784": "tusd",
         "0x03ab98f5dc94996F8C33E15cD4468794d12d41f9": "usdn",
-    }
-elif chain.id == 137: #poly
-    weth = Contract("0x7ceb23fd6bc0add59e62ac25578270cff1b9f619")
-    usdc = Contract("0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174")
-    dai = Contract("0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063")
-    wbtc = Contract("0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6")
-    usdt = Contract("0xc2132D05D31c914a87C6611C10748AEb04B58e8F")
-    wmatic = Contract("0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270")
-    
-    STABLECOINS = {
+    },
+    Network.Polygon: {
         "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174": "usdc",
         "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063": "dai",
         "0xc2132D05D31c914a87C6611C10748AEb04B58e8F": "usdt",
         "0xE840B73E5287865EEc17d250bFb1536704B43B21": "musd",
-    }
-elif chain.id == 250:
-    weth = Contract('0x74b23882a30290451A17c44f4F05243b6b58C76d')
-    usdc = Contract('0x04068DA6C83AFCFA0e13ba15A6696662335D5B75')
-    dai = Contract('0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E')
-    wbtc = Contract('0x321162Cd933E2Be498Cd2267a90534A804051b11')
-    usdt = Contract('0x049d68029688eAbF473097a2fC38ef61633A3C7A') #fusdt
-    wftm = Contract('0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83')
-
-    STABLECOINS = {
+    },
+    Network.Fantom: {
         "0x04068DA6C83AFCFA0e13ba15A6696662335D5B75": "usdc",
         "0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E": "dai",
         "0x049d68029688eAbF473097a2fC38ef61633A3C7A": "fusdt",
+    },
+    Network.Arbitrum: {
+        "": "",
+    },
+    Network.Avalanche: {
+        "": "",
     }
-elif chain.id == 42161:
-    STABLECOINS = {}
-elif chain.id == 43114:
-    STABLECOINS = {}
+}
 
+
+WRAPPED_GAS_COIN = {
+    Network.Mainnet: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    Network.BinanceSmartChain: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+    Network.Polygon: "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270",
+    Network.Fantom: "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83",
+    #Network.Arbitrum: "",
+    #Network.Avalanche: "",
+}.get(chain.id)
 
 
 
