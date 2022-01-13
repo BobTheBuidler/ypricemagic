4aaa3c4c42f6fa1dc76a56cb52686f8e61cb8737
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-01-13 22:51:56 +0000

    fix: wsteth exception type

--- ypricemagic/price_modules/wsteth.py
+++ ypricemagic/price_modules/wsteth.py
@@ -11,7 +11,7 @@ class wstEth:
         try: self.address = {
             Network.Mainnet: '0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0'
             }[chain.id]
-        except IndexError: self.address = None
+        except KeyError: self.address = None
     
     def get_price(self, block=None):
         share_price = raw_call(self.address, 'stEthPerToken()', output='int', block=block) / 1e18
