d946bc9f5b143a2f3652efdfd8fdb9048a539947
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-01-16 00:01:29 +0000

    feat: use raw_call for froyo

--- ypricemagic/price_modules/froyo.py
+++ ypricemagic/price_modules/froyo.py
@@ -1,12 +1,12 @@
 
 from brownie import chain
 from y.networks import Network
-from ypricemagic import Contract
+from ypricemagic.utils.raw_calls import raw_call
 
 
 def is_froyo(address):
     return chain.id == Network.Fantom and address == '0x4f85Bbf3B0265DCEd4Ec72ebD0358ccCf190F1B3'
 
 def get_price(froyo_address, block=None):
-    pool = Contract('0x83E5f18Da720119fF363cF63417628eB0e9fd523')
-    return pool.get_virtual_price(block_identifier = block) / 10 ** 18
+    pool = '0x83E5f18Da720119fF363cF63417628eB0e9fd523'
+    return raw_call(pool, "get_virtual_price()", block=block, output='int') / 1e18
