b704fb8ad86b78aae69673338cc717087f7a72f9
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-01-16 22:33:07 +0000

    feat: add xdai to Network

--- y/networks.py
+++ y/networks.py
@@ -1,28 +1,37 @@
 
 from enum import IntEnum
 
+from brownie import chain
+
 
 class Network(IntEnum):
     Mainnet = 1
     BinanceSmartChain = 56
+    xDai = 100
     Polygon = 137
     Fantom = 250
     Arbitrum = 42161
     Avalanche = 43114
 
     @staticmethod
-    def label(chain_id):
+    def label(chain_id = None):
+        if not chain_id: chain_id = chain.id
+
         if chain_id == Network.Mainnet:                 return 'ETH'
         elif chain_id == Network.BinanceSmartChain:     return 'BSC'
+        elif chain_id == Network.xDai:                  return 'XDAI'
         elif chain_id == Network.Fantom:                return 'FTM'
         elif chain_id == Network.Polygon:               return 'POLY'
         elif chain_id == Network.Arbitrum:              return 'ARRB'
         elif chain_id == Network.Avalanche:             return 'AVAX'
     
     @staticmethod
-    def name(chain_id):
+    def name(chain_id = None):
+        if not chain_id: chain_id = chain.id
+
         if chain_id == Network.Mainnet:                 return 'Mainnet'
         elif chain_id == Network.BinanceSmartChain:     return 'Binance Smart Chain'
+        elif chain_id == Network.xDai:                  return 'xDai'
         elif chain_id == Network.Fantom:                return 'Fantom'
         elif chain_id == Network.Polygon:               return 'Polygon'
         elif chain_id == Network.Arbitrum:              return 'Arbitrum'
