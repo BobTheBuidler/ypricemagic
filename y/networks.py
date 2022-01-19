792abeeaabc1abe5f6df82c049ae1ba332da1751
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-01-19 09:40:04 +0000

    feat: add network names + labels for movr, one, heco

--- y/networks.py
+++ y/networks.py
@@ -23,10 +23,13 @@ class Network(IntEnum):
         if chain_id == Network.Mainnet:                 return 'ETH'
         elif chain_id == Network.BinanceSmartChain:     return 'BSC'
         elif chain_id == Network.xDai:                  return 'XDAI'
-        elif chain_id == Network.Fantom:                return 'FTM'
+        elif chain_id == Network.Heco:                  return 'HECO'
         elif chain_id == Network.Polygon:               return 'POLY'
+        elif chain_id == Network.Fantom:                return 'FTM'
+        elif chain_id == Network.Moonriver:             return 'MOVR'
         elif chain_id == Network.Arbitrum:              return 'ARRB'
         elif chain_id == Network.Avalanche:             return 'AVAX'
+        elif chain_id == Network.Harmony:               return 'ONE'
     
     @staticmethod
     def name(chain_id = None):
@@ -35,7 +38,10 @@ class Network(IntEnum):
         if chain_id == Network.Mainnet:                 return 'Mainnet'
         elif chain_id == Network.BinanceSmartChain:     return 'Binance Smart Chain'
         elif chain_id == Network.xDai:                  return 'xDai'
-        elif chain_id == Network.Fantom:                return 'Fantom'
+        elif chain_id == Network.Heco:                  return 'Heco'
         elif chain_id == Network.Polygon:               return 'Polygon'
+        elif chain_id == Network.Fantom:                return 'Fantom'
+        elif chain_id == Network.Moonriver:             return 'Moonriver'
         elif chain_id == Network.Arbitrum:              return 'Arbitrum'
         elif chain_id == Network.Avalanche:             return 'Avalanche'
+        elif chain_id == Network.Harmony:               return "HarmonyOne Shard 0"
