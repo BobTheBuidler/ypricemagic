146d83805c1416cae88fe028ddf3a08698c06143
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-01-20 04:16:53 +0000

    feat: add some methods to erc20 class

--- ypricemagic/classes.py
+++ ypricemagic/classes.py
@@ -1,15 +1,32 @@
 from decimal import Decimal
+from functools import cached_property, lru_cache
 
-from y.contracts import Contract
+from y.contracts import Contract, build_name
 
-from ypricemagic.utils.raw_calls import _totalSupplyReadable
+from ypricemagic.utils.raw_calls import _name, _symbol, _totalSupplyReadable
 
 
+# WIP
+@lru_cache(maxsize=None) # TODO test to see this actually works how I want
 class ERC20:
     def __init__(self, token_address) -> None:
         self.address = token_address
-        self.contract = Contract(self.address)
-        self.build_name = self.contract.__dict__['_build']['contractName']
+    
+    @cached_property
+    def contract(self):
+        return Contract(self.address)
+
+    @cached_property
+    def build_name(self):
+        return build_name(self.address)
+    
+    @cached_property
+    def symbol(self):
+        return _symbol(self.address)
+    
+    @cached_property
+    def name(self):
+        return _name(self.address)
 
     def total_supply(self, block=None) -> Decimal:
         return _totalSupplyReadable(self.address, block)
