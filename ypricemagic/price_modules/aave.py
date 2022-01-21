10db4337524b91609c5a01f09bdb84b90d6af3d4
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-01-21 05:50:24 +0000

    fix: is_atoken for non verified pools

--- ypricemagic/price_modules/aave.py
+++ ypricemagic/price_modules/aave.py
@@ -1,22 +1,32 @@
-from y.contracts import Contract
+import logging
+from functools import lru_cache
+
+from y.contracts import has_method, has_methods
 from y.utils.cache import memory
 from ypricemagic import magic
 from ypricemagic.utils.raw_calls import raw_call
 
+logger = logging.getLogger(__name__)
 
+
+@lru_cache
 def is_atoken_v1(address):
-    contract = Contract(address)
-    required = {"getInterestRedirectionAddress", "getRedirectedBalance", "underlyingAssetAddress"}
-    return set(contract.__dict__) & required == required
+    logger.debug(f'Checking `is_atoken_v1({address})')
+    result = has_methods(address, ['getInterestRedirectionAddress()(address)','getRedirectedBalance()(uint)','underlyingAssetAddress()(address)'])
+    logger.debug(f'`is_atoken_v1({address}` returns `{result}`')
+    return result
 
+@lru_cache
 def is_atoken_v2(address):
-    contract = Contract(address)
-    return all([
-        hasattr(contract, 'UNDERLYING_ASSET_ADDRESS'),
-        any([hasattr(contract,'LENDING_POOL'), hasattr(contract, 'ATOKEN_REVISION')])
+    logger.debug(f'Checking `is_atoken_v2({address})')
+    result = all([
+        has_method(address, 'UNDERLYING_ASSET_ADDRESS()(address)'),
+        has_methods(address, ['LENDING_POOL()(address)','ATOKEN_REVISION()(address)'], any)
     ])
+    logger.debug(f'`is_atoken_v1({address}` returns `{result}`')
+    return result
 
-@memory.cache()
+@lru_cache
 def is_atoken(address):
     return is_atoken_v1(address) or is_atoken_v2(address)
 
