13e6acdbe5e89beaecdce1fe3ae656b0d244c3c8
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-01-23 06:00:16 +0000

    chore: cleanup piedao

--- ypricemagic/price_modules/piedao.py
+++ ypricemagic/price_modules/piedao.py
@@ -17,25 +17,17 @@ logger = logging.getLogger(__name__)
 @log(logger)
 @lru_cache
 def is_pie(address):
-    logger.debug(f'Checking `is_pie({address})')
     result = has_method(address, "getCap()(uint)")
-    logger.debug(f'`is_pie({address}` returns `{result}`')
     return result
 
 @log(logger)
 def get_price(pie_address: str, block=None):
-    logger.debug(f'fetching `piedao.get_price({pie_address}, {block})')
     price = tvl(pie_address, block) / totalSupplyReadable(pie_address, block)
-    logger.debug(f'`piedao.get_price({pie_address}, {block})` returns `{price}`')
     return price
 
 @log(logger)
 def get_tokens(pie_address: str, block: int = None):
-    logger.debug(f'fetching tokens for pie {pie_address} at block {block}')
-    tokens = Call(pie_address, ['getTokens()(address[])'], [['tokens',None]], _w3=web3, block_id=block)()['tokens']
-    tokens = [address for address in tokens if address != ZERO_ADDRESS]
-    logger.debug(f'pie tokens at block {block}: {tokens}')
-    return tokens
+    return Call(pie_address, ['getTokens()(address[])'], [['tokens',None]], _w3=web3, block_id=block)()['tokens']
 
 @log(logger)
 def get_bpool(pie_address: str, block: int = None):
@@ -58,12 +50,3 @@ def tvl(pie_address: str, block: int = None):
 
 # currently unused:
 PPROXYPAUSABLE = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"stateMutability":"payable","type":"fallback"},{"inputs":[{"internalType":"address","name":"_value","type":"address"}],"name":"addressToBytes32","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_value","type":"bytes32"}],"name":"bytes32ToAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_value","type":"bytes32"}],"name":"bytes32ToString","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"pure","type":"function"},{"inputs":[],"name":"getImplementation","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getPaused","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getPauzer","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getProxyOwner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"}],"name":"readAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"}],"name":"readBool","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"}],"name":"readString","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renouncePauzer","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_newImplementation","type":"address"}],"name":"setImplementation","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bool","name":"_value","type":"bool"}],"name":"setPaused","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_newPauzer","type":"address"}],"name":"setPauzer","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_newOwner","type":"address"}],"name":"setProxyOwner","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"}],"name":"storageRead","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"_value","type":"string"}],"name":"stringToBytes32","outputs":[{"internalType":"bytes32","name":"result","type":"bytes32"}],"stateMutability":"pure","type":"function"}]
-
-# forcing function to get price for wATRI, might not need this anymore
-@log(logger)
-def _value(token, block, balance):
-    logging.debug(f'token: {token}')
-    if token == '0xf037f37f58110933834CA64545E4ffD169736561': # wrapped ATRI
-        newtoken = '0xdacD69347dE42baBfAEcD09dC88958378780FB62' # ATRI
-        return balance / 10 ** _decimals(token) * magic.get_price(newtoken, block=block)
-    return balance / 10 ** _decimals(token) * magic.get_price(token, block=block)
