a65df1b66c3d38d644a6016e575bf8a6e980f5c6
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-01-23 08:19:41 +0000

    fix: has_method with return_response = True

--- y/contracts.py
+++ y/contracts.py
@@ -93,7 +93,7 @@ def has_method(address: str, method: str, return_response: bool = False) -> bool
     Checks to see if a contract has a `method` view method with no inputs.
     `return_response=True` will return `response` in bytes if `response` else `False`
     '''
-    try: response = Call(address, [f'{method}'], [['key', None]], _w3=web3)()['key']
+    try: response = Call(address, [method], [['key', None]], _w3=web3)()['key']
     except Exception as e:
         if call_reverted(e): return False
         # We return False here because `has_method` is only supposed to work for public view methods with no inputs
@@ -102,7 +102,7 @@ def has_method(address: str, method: str, return_response: bool = False) -> bool
         raise
     
     if response is None: return False
-    if return_response: return list(response)[0]
+    if return_response: return response
     return True
 
 @log(logger)
