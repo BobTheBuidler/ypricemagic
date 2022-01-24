61a34f4d10d52f8715c5b6433459ccc01d2dd047
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-01-24 04:14:11 +0000

    feat: use usdc.e for uniswap quotes on avax instead of usdc

--- y/constants.py
+++ y/constants.py
@@ -61,7 +61,7 @@ elif chain.id == Network.Avalanche:
     weth = Contract('0x49D5c2BdFfac6CE2BFdB6640F4F80f226bc10bAB')
     wbtc = Contract('0x50b7545627a5162F82A992c33b87aDc75187B218')
     dai = Contract('0xd586E7F844cEa2F87f50152665BCbc2C279D8d70')
-    usdc = Contract('0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E')
+    usdc = Contract('0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664') # usdc.e
     usdt = Contract('0x9702230A8Ea53601f5cD2dc00fDBc13d4dF4A8c7')
 
 elif chain.id == Network.Moonriver:
