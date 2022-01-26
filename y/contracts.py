f472126afe937662b1547f109227c93bb9493bd8
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-01-26 18:14:17 +0000

    fix: MessedUpBrownieContract assessment

--- y/contracts.py
+++ y/contracts.py
@@ -72,6 +72,9 @@ def Contract(address):
     with _contract_lock:
         try: return _contract(address)
         except CompilerError as e: raise MessedUpBrownieContract(address, str(e))
+        except IndexError as e:
+            if 'list index out of range' in str(e): raise MessedUpBrownieContract(address, str(e))
+            else: raise
         except ValueError as e:
             if contract_not_verified(e): raise ContractNotVerified(f'{address} on {Network.printable()}')
             if "invalid literal for int() with base 16" in str(e): raise MessedUpBrownieContract(address, str(e))
