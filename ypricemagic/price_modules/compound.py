c968d2d34825e3544a833cc066c7461f4cf673c6
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-01-28 08:19:35 +0000

    chore: refactor compound

--- ypricemagic/price_modules/compound.py
+++ ypricemagic/price_modules/compound.py
@@ -1,15 +1,17 @@
 import logging
 
-from brownie import chain, convert
-from y.contracts import Contract, has_methods
+from brownie import chain, convert, web3
+from multicall import Call, Multicall
+from y.constants import EEE_ADDRESS
+from y.contracts import has_methods
 from y.decorators import log
 from y.networks import Network
 from y.utils.logging import gh_issue_request
-from ypricemagic.utils.multicall import (fetch_multicall,
-                                         multicall_same_func_no_input)
 from ypricemagic.utils.raw_calls import _decimals, raw_call
 
 logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.INFO)
 
 TROLLERS = {
     Network.Mainnet: {
@@ -36,74 +38,69 @@ TROLLERS = {
 
 
 class Comptroller:
-    def __init__(self, address, markets) -> None:
-        self.address = address
-        self.markets = markets
+    def __init__(self, address: str = None, key: str = None) -> None:
+
+        assert address or key,          'Must provide either an address or a key'
+        assert not (address and key),   'Must provide either an address or a key, not both'
+
+        if key: address = TROLLERS[key]
+        else: key = [key for key in TROLLERS if self.address == TROLLERS[key]][0]
+
+        self.address = convert.to_address(address)
+        self.key = key
+        self.markets = {
+            convert.to_address(market)
+            for market
+            in Call(self.address, ["getAllMarkets()(address[])"],[['markets',None]], _w3=web3)()['markets']
+        }
+        logger.info(f"loaded {len(self.markets)} lending markets on {self.key}")
+    
+
+    @log(logger)
+    def __contains__(self, token_address: str) -> bool:
+        return convert.to_address(token_address) in self.markets
+
 
 class Compound:
     def __init__(self) -> None:
-        self.trollers = TROLLERS
-
-        if len(self.trollers) == 0: return 
-
-        response = multicall_same_func_no_input(self.trollers.values(), 'getAllMarkets()(address[])')
-        response = [[convert.to_address(market) for market in troller_market] for troller_market in response]
+        if len(TROLLERS) == 0: return 
         self.trollers = {
-            name: Comptroller(troller, markets)
-            for name, troller, markets
-            in zip(self.trollers.keys(), self.trollers.values(), response)
+            protocol: Comptroller(troller)
+            for protocol, troller
+            in TROLLERS.items()
         }
+
 
     @log(logger)
     def is_compound_market(self, token_address: str) -> bool:
-        if any(token_address in self.trollers[name].markets for name in self.trollers):
+        if any(token_address in troller for troller in self.trollers.values()):
             return True
+
         # NOTE: Workaround for pools that have since been revoked
         result = has_methods(token_address, ['isCToken()(bool)','comptroller()(address)','underlying()(address)'])
         if result is True: self.notify_if_unknown_comptroller(token_address)
         return result
     
+
     @log(logger)
     def __contains__(self, token_address: str) -> bool:
         return self.is_compound_market(token_address)
     
+
     @log(logger)
     def get_price(self, token_address: str, block=None):
-        token = Contract(token_address)
-        try:
-            underlying, exchange_rate, decimals = fetch_multicall(
-                [token, 'underlying'],
-                [token, 'exchangeRateCurrent'],
-                [token, 'decimals'],
-                block=block
-            )
-            exchange_rate /= 1e18
-            under_decimals = _decimals(underlying,block)
-            return [exchange_rate * 10 ** (decimals - under_decimals), underlying]
-        except AttributeError: # this will run for gas coin markets like cETH, crETH
-            exchange_rate, decimals = fetch_multicall(
-                [token, 'exchangeRateCurrent'],
-                [token, 'decimals'],
-                block=block
-            )
-            exchange_rate /= 1e18
-            under_decimals = 18
-            return [exchange_rate * 10 ** (decimals - under_decimals), "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"]
-        '''
-        # NOTE no current chains without multicall2 support, keeping for future use
+        methods = 'underlying()(address)','exchangeRateCurrent()(uint)','decimals()(uint)'
+        calls = [Call(token_address, [method], [[i,None]]) for i, method in enumerate(methods)]
+        underlying, exchange_rate, decimals = Multicall(calls, block_id=block, _w3=web3, require_success=False)().values()
 
-        try:
-            underlying = token.underlying(block_identifier = block)
-            exchange_rate = token.exchangeRateStored(block_identifier = block) / 1e18
-            decimals = _decimals(token_address,block)
-            under_decimals = _decimals(underlying,block)
-            return [exchange_rate * 10 ** (decimals - under_decimals), underlying]
-        except AttributeError:
-            exchange_rate = token.exchangeRateStored(block_identifier = block) / 1e18
-            decimals = _decimals(token_address,block)
-            under_decimals = 18
-            return [exchange_rate * 10 ** (decimals - under_decimals), "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"]
-        '''
+        # this will run for gas coin markets like cETH, crETH
+        if underlying is None: underlying, under_decimals = EEE_ADDRESS, 18
+        else: under_decimals = _decimals(underlying,block)
+        
+        exchange_rate /= 1e18
+
+        return [exchange_rate * 10 ** (decimals - under_decimals), underlying]
+
 
     @log(logger)
     def notify_if_unknown_comptroller(self, token_address: str) -> None:
