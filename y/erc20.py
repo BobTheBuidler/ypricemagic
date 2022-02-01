faf0fa0915a24f8c00109f7921a840ba4388492e
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-02-01 11:15:54 +0000

    feat: erc goodies

--- y/erc20.py
+++ y/erc20.py
@@ -1,13 +1,20 @@
+import logging
+from functools import cached_property, lru_cache
 from typing import Any, Callable, KeysView, List, Tuple, Union
 
 import brownie
+from brownie import convert
+from y.decorators import log
 from brownie.convert.datatypes import EthAddress
 from eth_typing.evm import Address, BlockNumber
+from ypricemagic import magic
 from ypricemagic.utils.multicall import (multicall_decimals,
                                          multicall_totalSupply)
 from ypricemagic.utils.raw_calls import _decimals, _totalSupply
 
 from y.contracts import Contract
+
+logger = logging.getLogger(__name__)
 
 SUPPORTED_INPUT_TYPES = str, Address, EthAddress, brownie.Contract, Contract
 
@@ -78,3 +85,35 @@ def _check_if_supported(
     
     if type(input) not in SUPPORTED_INPUT_TYPES:
         raise TypeError(f'Unsupported input type: {type(input)}')
+
+
+class ERC20:
+    def __init__(self, address: str) -> None:
+        self.address = convert.to_address(address)
+        pass
+
+    @log(logger)
+    @lru_cache
+    def decimals(self, block: int = None) -> int:
+        return decimals(self.address, block=block)
+    
+    @log(logger)
+    def scale(self, block: int = None) -> int:
+        return 10 ** self.decimals(block=block)
+
+    @log(logger)
+    @lru_cache
+    def total_supply(self, block: int = None) -> int:
+        return totalSupply(self.address, block=block)
+    
+    @log(logger)
+    def total_supply_readable(self, block: int = None) -> float:
+        return self.total_supply(block=block) / self.scale(block=block)
+
+    @log(logger)
+    def price(self, block: int = None, return_None_on_failure: bool = False) -> float:
+        return magic.get_price(
+            self.address, 
+            block=block, 
+            fail_to_None=return_None_on_failure
+        )
