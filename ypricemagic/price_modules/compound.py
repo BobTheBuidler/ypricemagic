2de9c49bcf8c4eecd9622b9b56172e81f659824e
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-02-01 02:23:09 +0000

    feat: cream on polygon

--- ypricemagic/price_modules/compound.py
+++ ypricemagic/price_modules/compound.py
@@ -28,6 +28,7 @@ TROLLERS = {
         "easyfi":           "0xcb3fA413B23b12E402Cfcd8FA120f983FB70d8E8",
         "apple":            "0x46220a07F071D1a821D68fA7C769BCcdA3C65430",
         "chumhum":          "0x1D43f6DA91e9EF6614dCe95bCef43E4d7b2bcFB5",
+        "cream":            "0x20CA53E2395FA571798623F1cFBD11Fe2C114c24",
     },
     Network.Fantom: {
         "cream":            "0x4250A6D3BD57455d7C6821eECb6206F507576cD2",
