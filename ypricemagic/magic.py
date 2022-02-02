8e51bde042151b5c1482c6758bd867252346f2ef
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-02-02 23:43:57 +0000

    chore: add deprecation warning

---
+++ ypricemagic/magic.py
@@ -0,0 +1,13 @@
+
+import logging
+
+from y import magic
+
+logger = logging.getLogger(__name__)
+
+def get_price(token: str, block: int = None) -> float:
+    logger.warn('ypricemagic is in the process of being migrated to y.'
+                'y can do all of the same old stuff you expect, plus some new stuff.'
+                'This method still works for now, but will be removed soon.'
+                'Please update your scripts to use `y.get_price(token, block)`.')
+    return magic.get_price(token, block)
