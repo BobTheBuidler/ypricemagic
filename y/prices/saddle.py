e1b04a16113d9befb06ed1928f100d9c9480aa33
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-03-17 17:20:46 +0000

    feat: support new saddle pools

--- y/prices/saddle.py
+++ y/prices/saddle.py
@@ -3,9 +3,11 @@ import logging
 from functools import lru_cache
 from typing import List
 
+from brownie import chain
 from y.contracts import has_method, has_methods
 from y.decorators import log
 from y.erc20 import decimals
+from y.networks import Network
 from y.prices import magic
 from y.utils.multicall import \
     multicall_same_func_same_contract_different_inputs
@@ -24,10 +26,13 @@ def is_saddle_lp(token_address: str) -> bool:
 @lru_cache
 @log(logger)
 def get_pool(token_address: str) -> str:
-    if token_address == '0xc9da65931ABf0Ed1b74Ce5ad8c041C4220940368': # saddle aleth doesn't have swap() function
-        return '0xa6018520EAACC06C30fF2e1B3ee2c7c22e64196a'
-    elif token_address == '0xd48cF4D7FB0824CC8bAe055dF3092584d0a1726A': # saddle d4
-        return '0xC69DDcd4DFeF25D8a793241834d4cc4b3668EAD6'
+    if chain.id == Network.Mainnet:
+        if token_address == '0xc9da65931ABf0Ed1b74Ce5ad8c041C4220940368': # saddle aleth doesn't have swap() function
+            return '0xa6018520EAACC06C30fF2e1B3ee2c7c22e64196a'
+        elif token_address == '0xd48cF4D7FB0824CC8bAe055dF3092584d0a1726A': # saddle d4
+            return '0xC69DDcd4DFeF25D8a793241834d4cc4b3668EAD6'
+        elif token_address == '0xF32E91464ca18fc156aB97a697D6f8ae66Cd21a3':
+            return '0xdf3309771d2BF82cb2B6C56F9f5365C8bD97c4f2'
     pool = has_method(token_address, 'swap()(address)', return_response=True)
     return pool or None
 
