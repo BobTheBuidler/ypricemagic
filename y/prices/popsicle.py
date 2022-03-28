9081e58d322b248479f468c278fb9024aa5438b3
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-03-28 04:23:31 +0000

    feat: split get_price into functional parts

--- y/prices/popsicle.py
+++ y/prices/popsicle.py
@@ -1,12 +1,12 @@
 import logging
 from functools import lru_cache
-from typing import Optional
+from typing import Optional, Tuple
 
 from multicall import Call, Multicall
 from y import convert
 from y.classes.common import WeiBalance
 from y.contracts import has_methods
-from y.datatypes import UsdPrice
+from y.datatypes import UsdPrice, UsdValue
 from y.decorators import log
 from y.typing import AnyAddressType, Block
 from y.utils.raw_calls import _totalSupplyReadable
@@ -22,11 +22,22 @@ def is_popsicle_lp(token_address: AnyAddressType) -> bool:
 @log(logger)
 def get_price(token: AnyAddressType, block: Optional[Block] = None) -> UsdPrice:
     address = convert.to_address(token)
+    balance0, balance1 = get_balances(token, block)
+    total_val = balance0.value_usd(block) + balance1.value_usd(block)
+    total_supply = _totalSupplyReadable(address,block)
+    return UsdPrice(total_val / total_supply)
+
+@log(logger)
+def get_tvl(token: AnyAddressType, block: Optional[Block] = None) -> UsdValue:
+    balance0, balance1 = get_balances(token, block)
+    return UsdValue(balance0.value_usd() + balance1.value_usd())
+
+@log(logger)
+def get_balances(token: AnyAddressType, block: Optional[Block] = None) -> Tuple[WeiBalance,WeiBalance]:
+    address = convert.to_address(token)
     methods = 'token0()(address)','token1()(address)','usersAmounts()((uint,uint))'
     calls = [Call(address, method, [[method,None]]) for method in methods]
     token0, token1, (balance0, balance1) = Multicall(calls, block_id=block)().values()
     balance0 = WeiBalance(balance0, token0, block=block)
     balance1 = WeiBalance(balance1, token1, block=block)
-    total_val = balance0.value_usd() + balance1.value_usd()
-    total_supply = _totalSupplyReadable(address,block)
-    return UsdPrice(total_val / total_supply)
+    return balance0, balance1
