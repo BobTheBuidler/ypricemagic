de50d82cf8d286175ce562915a3e0142b4f820d5
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-03-28 04:19:50 +0000

    feat: use WeiBalance class for caching and readability improvements

--- y/prices/popsicle.py
+++ y/prices/popsicle.py
@@ -2,14 +2,14 @@ import logging
 from functools import lru_cache
 from typing import Optional
 
-import y.prices.magic
 from multicall import Call, Multicall
 from y import convert
+from y.classes.common import WeiBalance
 from y.contracts import has_methods
 from y.datatypes import UsdPrice
 from y.decorators import log
 from y.typing import AnyAddressType, Block
-from y.utils.raw_calls import _decimals, _totalSupplyReadable, raw_call
+from y.utils.raw_calls import _totalSupplyReadable
 
 logger = logging.getLogger(__name__)
 
@@ -25,8 +25,8 @@ def get_price(token: AnyAddressType, block: Optional[Block] = None) -> UsdPrice:
     methods = 'token0()(address)','token1()(address)','usersAmounts()((uint,uint))'
     calls = [Call(address, method, [[method,None]]) for method in methods]
     token0, token1, (balance0, balance1) = Multicall(calls, block_id=block)().values()
-    balance0 /= 10 ** _decimals(token0,block)
-    balance1 /= 10 ** _decimals(token1,block)
+    balance0 = WeiBalance(balance0, token0, block=block)
+    balance1 = WeiBalance(balance1, token1, block=block)
+    totalVal = balance0.value_usd() + balance1.value_usd()
     totalSupply = _totalSupplyReadable(address,block)
-    totalVal = balance0 * y.prices.magic.get_price(token0,block) + balance1 * y.prices.magic.get_price(token1,block)
     return UsdPrice(totalVal / totalSupply)
