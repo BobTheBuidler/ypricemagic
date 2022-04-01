72007bfafca4839c68b895dad122f16b8b46b837
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-04-01 20:27:45 +0000

    fix: rate limiting during imports

--- y/utils/cache.py
+++ y/utils/cache.py
@@ -1,4 +1,10 @@
-from joblib import Memory
 from brownie import chain
+from joblib import Memory
+from y.decorators import auto_retry
 
-memory = Memory(f"cache/{chain.id}", verbose=0)
+
+@auto_retry
+def _memory():
+    return Memory(f"cache/{chain.id}", verbose=0)
+
+memory = _memory()
