b013ed036ee89b1d53eeabcb6910e19ecc97e1f8
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-04-05 14:01:30 +0000

    fix: popsicle when get_balances returns None

--- y/prices/popsicle.py
+++ y/prices/popsicle.py
@@ -20,19 +20,24 @@ def is_popsicle_lp(token_address: AnyAddressType) -> bool:
     return has_methods(token_address, ['token0()(address)','token1()(address)','usersAmounts()((uint,uint))'])
 
 @log(logger)
-def get_price(token: AnyAddressType, block: Optional[Block] = None) -> UsdPrice:
+def get_price(token: AnyAddressType, block: Optional[Block] = None) -> Optional[UsdPrice]:
     address = convert.to_address(token)
     total_val = get_tvl(address, block)
+    if total_val is None:
+        return None
     total_supply = _totalSupplyReadable(address,block)
     return UsdPrice(total_val / total_supply)
 
 @log(logger)
-def get_tvl(token: AnyAddressType, block: Optional[Block] = None) -> UsdValue:
-    balance0, balance1 = get_balances(token, block)
+def get_tvl(token: AnyAddressType, block: Optional[Block] = None) -> Optional[UsdValue]:
+    balances = get_balances(token, block)
+    if balances is None:
+        return None
+    balance0, balance1 = balances
     return UsdValue(balance0.value_usd() + balance1.value_usd())
 
 @log(logger)
-def get_balances(token: AnyAddressType, block: Optional[Block] = None) -> Tuple[WeiBalance,WeiBalance]:
+def get_balances(token: AnyAddressType, block: Optional[Block] = None) -> Optional[Tuple[WeiBalance,WeiBalance]]:
     address = convert.to_address(token)
     methods = 'token0()(address)','token1()(address)','usersAmounts()((uint,uint))'
     calls = [Call(address, method, [[method,None]]) for method in methods]
