65cfa372db8ebe0cf9b6a7c935cf72301919ce27
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-05-06 00:18:16 +0000

    feat: implement eth_retry

--- y/utils/cache.py
+++ y/utils/cache.py
@@ -1,9 +1,9 @@
+import eth_retry
 from brownie import chain
 from joblib import Memory
-from y.decorators import auto_retry
 
 
-@auto_retry
+@eth_retry.auto_retry
 def _memory():
     return Memory(f"cache/{chain.id}", verbose=0)
 
