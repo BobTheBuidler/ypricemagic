bff2584a04d6d114d77f2231ccb081486ca7db8e
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-08-08 07:57:42 +0000

    feat: singleton lock

--- y/classes/singleton.py
+++ y/classes/singleton.py
@@ -1,4 +1,5 @@
 
+import threading
 from typing import Any, Optional, TypeVar
 
 T = TypeVar("T", bound=type)
@@ -7,9 +8,13 @@ T = TypeVar("T", bound=type)
 class Singleton(type):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         self.__instance: Optional[T] = None
+        self.__lock == threading.Lock()
         super().__init__(*args, **kwargs)
 
     def __call__(self, *args: Any, **kwargs: Any) -> T:
         if self.__instance is None:
-            self.__instance = super().__call__(*args, **kwargs)
-        return self.__instance
+            with self.__lock:
+                # Check again in case `__instance` was set while we were waiting for the lock.
+                if self.__instance is None:
+                    self.__instance = super().__call__(*args, **kwargs)
+                return self.__instance
