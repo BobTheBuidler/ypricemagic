611904971aa0cf6d30fcc366e1412a7f8847962a
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2022-11-17 18:41:40 -0500

    fix: get_ethereum_client_async (#165)

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         'bobs_lazy_logging==0.0.4',
         'cachetools>=4.1.1',
         'checksum_dict>=1.1.1',
-        'dank_mids>=4.20.10',
+        'dank_mids>=4.20.17',
         'eth-brownie>=1.18.1,<1.20',
         'eth_retry>=0.1.10,<0.2',
         'joblib>=1.0.1',
