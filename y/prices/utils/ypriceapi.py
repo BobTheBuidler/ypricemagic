082c56c9f5aed89b4dc9886272172f60034463ed
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2023-01-19 20:00:11 -0500

    fix: implement ypriceapi semaphore (#200)

--- y/prices/utils/ypriceapi.py
+++ y/prices/utils/ypriceapi.py
@@ -1,13 +1,15 @@
 
+import asyncio
 import logging
 import os
-
-from aiohttp import ClientSession, TCPConnector, BasicAuth
-from y.utils.dank_mids import dank_w3
-from y.datatypes import Address,Block
-from brownie import chain
 from typing import Optional
+
+from aiohttp import BasicAuth, ClientSession, TCPConnector
+from brownie import chain
+
 from y.classes.common import UsdPrice
+from y.datatypes import Address, Block
+from y.utils.dank_mids import dank_w3
 
 logger = logging.getLogger(__name__)
 
@@ -22,6 +24,8 @@ notified = set()
 
 auth = BasicAuth(YPRICEAPI_USER, YPRICEAPI_PASS) if YPRICEAPI_USER and YPRICEAPI_PASS else None
 
+ypriceapi_semaphore = asyncio.Semaphore(50)
+
 async def get_price_from_api(
     token: Address,
     block: Optional[Block]
@@ -33,27 +37,35 @@ async def get_price_from_api(
     
     if block is None:
         block = await dank_w3.eth.block_number
+    
+    async with ypriceapi_semaphore:
+        while True:
+            async with ClientSession(YPRICEAPI_URL, connector=TCPConnector(verify_ssl=False), auth=auth) as session:
+                response = await session.get(f'/get_price/{chain.id}/{token}/{block}')
 
-    async with ClientSession(YPRICEAPI_URL, connector=TCPConnector(verify_ssl=False), auth=auth) as session:
-        response = await session.get(f'/get_price/{chain.id}/{token}/{block}')
+                # Handle successful response
+                if response.status == 200:
+                    result = await response.json()
+                    if isinstance(result, str):
+                        logger.error(f"Failed to get price from API: {result}")
+                        return None
+                    else:
+                        return result
 
-        # Handle successful response
-        if response.status == 200:
-            result = await response.json()
-            if isinstance(result, str):
-                logger.error(f"Failed to get price from API: {result}")
-                return None
-            else:
-                return result
-
-        # Handle unsuccessful response
-        # Unauthorized
-        elif response.status == 401:
-            if 401 not in notified:
-                logger.error('Your provided ypriceAPI user and password pair is not authorized for use. Falling back to your own node for pricing.')
-                notified.add(401)
-            return None
+                # Handle unsuccessful response
+                # Unauthorized
+                elif response.status == 401:
+                    if 401 not in notified:
+                        logger.error('Your provided ypriceAPI user and password pair is not authorized for use. Falling back to your own node for pricing.')
+                        notified.add(401)
+                    return None
+                
+                # Bad Gateway
+                elif response.status == 502:
+                    logger.error("ypriceAPI Bad Gateway. Retrying.")
+                    # Just retry
+                    pass
         
-        else:
-            raise NotImplementedError(f"status code {response.status} is not yet handled. Please tell Bob to fix this.")
+                else:
+                    raise NotImplementedError(f"status code {response.status} is not yet handled. Please tell Bob to fix this.")
             
