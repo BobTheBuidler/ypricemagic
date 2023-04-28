402819758a0d9a5d5c007b1a630fa7b81ddd95e1
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2023-04-28 00:04:22 -0400

    Fix 503 reason (#261)
    * fix: response reason
    
    * fix: race condition

--- y/prices/utils/ypriceapi.py
+++ y/prices/utils/ypriceapi.py
@@ -98,15 +98,16 @@ async def read_response(token: Address, block: Optional[Block], response: Client
     elif response.status == HTTPStatus.SERVICE_UNAVAILABLE:
         logger.warning(f"ypriceAPI returned status code {_get_err_reason(response)}:")
         try:
-            logger.warning(await response.json(content_type=None))
+            logger.warning(response.reason or await response.json(content_type=None))
         except Exception as e:
             logger.warning(f'exception decoding ypriceapi 503 response.{FALLBACK_STR}', exc_info=True)
         five_minutes = 60 * 5  # some arbitrary amount of time in case the header is missing on unexpected 503s
         retry_after = response.headers.get("Retry-After", five_minutes)
         logger.info(f"Falling back to your node for {retry_after/60} minutes.")
         global resume_at
-        resume_at = time() + retry_after
-                    
+        if (resume_from_this_err_at := time() + retry_after) > resume_at:
+            resume_at = resume_from_this_err_at
+
     else:
         logger.warning(f'ypriceAPI returned status code {_get_err_reason(response)} for {token} at {block}.{FALLBACK_STR}')
             
