b9d125d37d46b926ffb9e6c5db308d195fea6552
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-08-18 05:27:08 +0000

    chore: add anyETH to sense check

--- y/prices/utils/sense_check.py
+++ y/prices/utils/sense_check.py
@@ -102,6 +102,8 @@ ACCEPTABLE_HIGH_PRICES = {
         "0x08FC9Ba2cAc74742177e0afC3dC8Aed6961c24e7", # ibbtcb
     ],
     Network.Fantom: [
+        # eth and eth-like
+        "0xBDC8fd437C489Ca3c6DA3B5a336D11532a532303", # anyeth
         # btc and btc-like
         "0x2406dCe4dA5aB125A18295f4fB9FD36a0f7879A2", # anybtc
         # other
