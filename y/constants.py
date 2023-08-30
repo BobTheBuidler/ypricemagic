7b25bf20040e95f9bdca62b7721345e99e92d1f2
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2023-08-30 15:20:33 -0400

    chore: comment

--- y/constants.py
+++ y/constants.py
@@ -114,7 +114,7 @@ elif chain.id == Network.Optimism:
 elif chain.id == Network.Base:
     dai  = Contract('0x50c5725949A6F0c72E6C4a641F24049A917DB0Cb')
     weth = Contract('0x4200000000000000000000000000000000000006')
-    usdc = Contract('0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA')
+    usdc = Contract('0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA') # usdbc
     usdt = Contract('0x4A3A6Dd60A34bB2Aba60D73B4C88315E9CeB6A3D')
     wbtc = Contract('0x77852193BD608A518dd7b7C2f891A1d02ceeB4d4') #temp placeholder, this is just some junk token. shouldnt impact results but we want wbtc when its avail
 
