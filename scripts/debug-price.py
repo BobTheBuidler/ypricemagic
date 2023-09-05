b8ea5f2acdd273f8b6ddcb16f06337a3d5e7a1a3
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-09-05 20:06:52 +0000

    chore: cleanup debug script

--- scripts/debug-price.py
+++ scripts/debug-price.py
@@ -2,16 +2,29 @@
 import logging
 import os
 
-import y
+from brownie import chain
+
+from y import get_price,ERC20, Network
+
 
 def main():
-    BAD = os.environ.get("BAD")
-    BLOCK = os.environ.get("BLOCK")
-    if not BAD:
+    bad = os.environ.get("BAD")
+    block = os.environ.get("BLOCK")
+    if not bad:
         raise ValueError("You must specify a token to debug by setting BAD env var")
-    if not BLOCK:
-        raise ValueError("You must specify a block to debug at by setting BLOCK env var")
+    if block:
+        block = int(block)
+    else:
+        block = chain.height
+        print('No block specified, using {block}')
+
+    print(f"Debugging price for {ERC20(bad).symbol} {bad} on {Network.name()} at block {block}")
+
     logger = logging.getLogger('y')
     logger.setLevel(logging.DEBUG)
     logger.addHandler(logging.StreamHandler())
-    y.get_price(BAD, int(BLOCK))
+    if not bool(os.environ.get('SHOW_EVENTS')):
+        logger = logging.getLogger('y.prices.dex.uniswap.v2.Pools').setLevel(logging.INFO)
+        logger = logging.getLogger('y.prices.dex.uniswap.v3.Pools').setLevel(logging.INFO)
+        logger = logging.getLogger('y.prices.dex.uniswap.velodrome.Pools').setLevel(logging.INFO)
+    get_price(bad, int(block))
