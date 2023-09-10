5bbcf17bc31c54b5c8400faffc19019286077a81
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-09-10 01:34:32 +0000

    fix: module AttributeError

--- y/prices/utils/buckets.py
+++ y/prices/utils/buckets.py
@@ -5,6 +5,7 @@ from typing import Awaitable, Callable, Tuple
 import a_sync
 
 from y import convert
+import y._db.utils as db
 from y.constants import STABLECOINS
 from y.datatypes import Address, AnyAddressType
 from y.prices import convex, one_to_one, popsicle, solidex, yearn
@@ -37,7 +38,7 @@ async def check_bucket(
     token_address = convert.to_address(token)
     logger = get_price_logger(token_address, block=None, extra='buckets')
     
-    bucket = await _db.utils._get_token_bucket(token_address)
+    bucket = await db._get_token_bucket(token_address)
     if bucket:
         logger.debug('returning bucket %s from ydb', bucket)
         return bucket
@@ -46,7 +47,7 @@ async def check_bucket(
     for bucket, check in string_matchers.items():
         if check(token):
             logger.debug(f"{token_address} is {bucket}")
-            await _db.utils._set_token_bucket(token, bucket)
+            await db._set_token_bucket(token, bucket)
             return bucket
         else:
             logger.debug(f"{token_address} is not {bucket}")
@@ -65,7 +66,7 @@ async def check_bucket(
             logger.debug(f"{token_address} is {bucket}")
             for fut in futs:
                 fut.cancel()
-            await _db.utils._set_token_bucket(token, bucket)
+            await db._set_token_bucket(token, bucket)
             return bucket
         else:
             logger.debug(f"{token_address} is not {bucket}")
@@ -103,7 +104,7 @@ async def check_bucket(
         bucket = 'curve lp'
     if bucket:
         logger.debug(f"{token_address} bucket is {bucket}")
-        await _db.utils._set_token_bucket(token, bucket)
+        await db._set_token_bucket(token, bucket)
     return bucket
 
 # these require neither calls to the chain nor contract initialization, just string comparisons (pretty sure)
