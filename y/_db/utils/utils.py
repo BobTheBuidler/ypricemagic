1f6603fc25adec02f70a2e43ea1d74e7e2436720
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-10-01 18:56:14 +0000

    fix: InterfaceError

--- y/_db/utils/utils.py
+++ y/_db/utils/utils.py
@@ -4,11 +4,11 @@ from contextlib import suppress
 
 from a_sync import a_sync
 from brownie import chain
-from pony.orm import TransactionIntegrityError, commit, db_session
+from pony.orm import (InterfaceError, TransactionIntegrityError, commit,
+                      db_session)
 
 from y._db.entities import Block, Chain
 from y._db.utils.common import executor
-
 
 logger = logging.getLogger(__name__)
 
@@ -29,7 +29,7 @@ def get_block(number: int) -> Block:
     chain = get_chain(sync=True)
     if block := Block.get(chain=chain, number=number):
         return block
-    with suppress(TransactionIntegrityError):
+    with suppress(TransactionIntegrityError, InterfaceError):
         block = Block(chain=chain, number=number)
         commit()
-    return Block.get(chain=get_chain(sync=True), number=number)
+    return get_block(number, sync=True)
