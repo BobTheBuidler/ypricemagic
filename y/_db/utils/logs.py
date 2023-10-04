a25e324ce5b00ec608a7c8fd2c2c516b3b009514
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-10-04 22:45:28 +0000

    feat: log select sql

--- y/_db/utils/logs.py
+++ y/_db/utils/logs.py
@@ -97,19 +97,19 @@ class LogCache(DiskCache[LogReceipt, LogCacheInfo]):
     @db_session
     def select(self, from_block: int, to_block: int) -> List[LogReceipt]:
         from y._db.utils import utils as db
-        return [
-            json.decode(log) for log in select(
-                log.raw for log in Log 
-                if log.block.chain == db.get_chain(sync=True)
-                and (not self.addresses or log.address in self.addresses)
-                and (self.topic0 is None or log.topic0 in self.topic0)
-                and (self.topic1 is None or log.topic1 in self.topic1)
-                and (self.topic2 is None or log.topic2 in self.topic2)
-                and (self.topic3 is None or log.topic3 in self.topic3)
-                and log.block.number >= from_block
-                and log.block.number <= to_block
-            )
-        ]
+        query = select(
+            log.raw for log in Log 
+            if log.block.chain == db.get_chain(sync=True)
+            and (not self.addresses or log.address in self.addresses)
+            and (self.topic0 is None or log.topic0 in self.topic0)
+            and (self.topic1 is None or log.topic1 in self.topic1)
+            and (self.topic2 is None or log.topic2 in self.topic2)
+            and (self.topic3 is None or log.topic3 in self.topic3)
+            and log.block.number >= from_block
+            and log.block.number <= to_block
+        )
+        logger.info(query.get_sql())
+        return [json.decode(log) for log in query]
     
     @db_session
     @retry_locked
