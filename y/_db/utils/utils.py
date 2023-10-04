57de5c1c9ced3917bab0cac8ff52b609b4081883
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-10-04 06:45:43 +0000

    fix: database is locked

--- y/_db/utils/utils.py
+++ y/_db/utils/utils.py
@@ -4,8 +4,8 @@ from contextlib import suppress
 
 from a_sync import a_sync
 from brownie import chain
-from pony.orm import (InterfaceError, TransactionIntegrityError, commit,
-                      db_session)
+from pony.orm import (InterfaceError, OperationalError,
+                      TransactionIntegrityError, commit, db_session)
 
 from y._db.common import executor
 from y._db.entities import Block, Chain
@@ -15,13 +15,20 @@ logger = logging.getLogger(__name__)
 @a_sync(default='async', executor=executor)
 @db_session
 def get_chain() -> Chain:
-    if c:=Chain.get(id=chain.id):
-        return c
-    with suppress(TransactionIntegrityError):
-        Chain(id=chain.id)
-        commit()
-        logger.debug('chain %s added to ydb', chain.id)
-    return Chain.get(id=chain.id)
+    try:
+        if c:=Chain.get(id=chain.id):
+            return c
+        with suppress(TransactionIntegrityError):
+            Chain(id=chain.id)
+            commit()
+            logger.debug('chain %s added to ydb', chain.id)
+        return Chain.get(id=chain.id)
+    except TransactionIntegrityError:
+        return get_chain(sync=False)
+    except OperationalError as e:
+        if "database is locked" not in str(e):
+            raise e
+        return get_chain(sync=False)
 
 @a_sync(default='async', executor=executor)
 @db_session
@@ -30,6 +37,10 @@ def get_block(number: int) -> Block:
     if block := Block.get(chain=chain, number=number):
         return block
     with suppress(TransactionIntegrityError, InterfaceError):
-        block = Block(chain=chain, number=number)
-        commit()
+        try:
+            block = Block(chain=chain, number=number)
+            commit()
+        except OperationalError as e:
+            if "database is locked" not in str(e):
+                raise e
     return get_block(number, sync=True)
