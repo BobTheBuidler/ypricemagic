53b0b3c35302e270b5d907f418d3073aa8b9c228
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2023-10-09 21:45:33 -0400

    fix: debugger util (#396)
    * fix: dont cache debug values
    
    * feat: make block optional for debugger

--- scripts/debug-price.py
+++ scripts/debug-price.py
@@ -2,7 +2,10 @@
 import logging
 import os
 
+from brownie import chain
+
 import y
+
 
 def main():
     BAD = os.environ.get("BAD")
@@ -10,8 +13,10 @@ def main():
     if not BAD:
         raise ValueError("You must specify a token to debug by setting BAD env var")
     if not BLOCK:
+        BLOCK = chain.height
+        logger.warning("no BLOCK specified, using %s", BLOCK)
         raise ValueError("You must specify a block to debug at by setting BLOCK env var")
     logger = logging.getLogger('y')
     logger.setLevel(logging.DEBUG)
     logger.addHandler(logging.StreamHandler())
-    y.get_price(BAD, int(BLOCK))
+    y.get_price(BAD, int(BLOCK), skip_cache=True)
