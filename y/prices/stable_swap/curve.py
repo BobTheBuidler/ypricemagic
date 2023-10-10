4f10061135c9e55aa1de181572910891afe0f760
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-10-10 01:47:37 +0000

    fix: Attr err

--- y/prices/stable_swap/curve.py
+++ y/prices/stable_swap/curve.py
@@ -70,7 +70,7 @@ _LT = TypeVar("_LT", bound=_CurveLoader)
 class _CurveEventsLoader(_CurveLoader, _EventsLoader):
     _events: "CurveEvents"
 
-class CurveEvents(ProcessedEvents[None]):
+class CurveEvents(ProcessedEvents[_EventItem]):
     __slots__ = "_base",
     def __init__(self, base: _LT):
         super().__init__(addresses=base.address)
@@ -86,6 +86,7 @@ class AddressProviderEvents(CurveEvents):
         elif event.name == 'AddressModified' and event['new_address'] != ZERO_ADDRESS:
             self.provider.identifiers[Ids(event['id'])].append(event['new_address'])
         logger.debug("%s loaded event %s at block %s", self, event, event.block_number)
+        return event
         
 class RegistryEvents(CurveEvents):
     __slots__ = "_tasks",
@@ -104,6 +105,7 @@ class RegistryEvents(CurveEvents):
         elif event.name == 'PoolRemoved':
             self.registry.curve.registries[event.address].discard(event['pool'])
         logger.debug("%s loaded event %s at block %s", self, event, event.block_number)
+        return event
     async def _add_pool(self, pool: Address) -> EthAddress:
         lp_token = await self.registry.contract.get_lp_token.coroutine(pool)
         self.registry.curve.token_to_pool[lp_token] = pool
