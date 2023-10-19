4b92152224cc03cfade19d59792b9bb7cb9bb6ee
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-10-19 23:12:32 +0000

    fix: decimal.InvalidOperation in db.set_price

--- y/_db/utils/price.py
+++ y/_db/utils/price.py
@@ -1,14 +1,15 @@
 
 import asyncio
-from decimal import Decimal
+from contextlib import suppress
+from decimal import Decimal, InvalidOperation
 from typing import List, Optional
 
 from a_sync import a_sync
-from pony.orm import TransactionIntegrityError, commit, db_session
+from pony.orm import db_session
 
 from y import constants
 from y._db.common import token_attr_threads
-from y._db.entities import Price, retry_locked, insert
+from y._db.entities import Price, insert, retry_locked
 from y._db.utils._ep import _get_get_block, _get_get_token
 
 
@@ -37,15 +38,16 @@ async def set_price(address: str, block: int, price: Decimal) -> None:
 @db_session
 @retry_locked
 def _set_price(address: str, block: int, price: Decimal) -> None:
-    get_block = _get_get_block()
-    get_token = _get_get_token()
-    if address == constants.EEE_ADDRESS:
-        address = constants.WRAPPED_GAS_COIN
-    insert(
-        type = Price,
-        block = get_block(block, sync=True),
-        token = get_token(address, sync=True),
-        price = Decimal(price),
-    )
-    
+    with suppress(InvalidOperation): # happens with really big numbers sometimes. nbd, we can just skip the cache in this case.
+        get_block = _get_get_block()
+        get_token = _get_get_token()
+        if address == constants.EEE_ADDRESS:
+            address = constants.WRAPPED_GAS_COIN
+        insert(
+            type = Price,
+            block = get_block(block, sync=True),
+            token = get_token(address, sync=True),
+            price = Decimal(price),
+        )
+        
 _tasks: List[asyncio.Task] = []
