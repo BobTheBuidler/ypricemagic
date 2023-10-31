ec16e5156974613d75c239361b3dfbb9d6c9c703
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-10-31 20:02:13 +0000

    feat: cache camelCase -> snake_case conversion

--- y/_db/structs.py
+++ y/_db/structs.py
@@ -1,13 +1,18 @@
 
+from functools import lru_cache
 from typing import List, Optional
 
-import inflection
 from dank_mids.types import _DictStruct
+from inflection import underscore
 
 
 class _CamelDictStruct(_DictStruct, rename="camel"):
+    """
+    A mixin class that allows camelCase key lookups for snake_case struct attrs
+    Original use case was so Log structs can be used interchangably with LogReceipt instances
+    """
     def __getitem__(self, attr: str):
-        return super().__getitem__(inflection.underscore(attr))
+        return getattr(self, _make_snake(attr))
 
 class Log(_CamelDictStruct):
     removed: Optional[bool]
@@ -23,3 +28,7 @@ class Log(_CamelDictStruct):
 class Trace(_CamelDictStruct):
     # TODO so we can trace chain for eth_port on alt chains
     pass
+
+@lru_cache(maxsize=None)
+def _make_snake(camel: str) -> str:
+    return underscore(camel)
