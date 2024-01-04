41d5581ee72a22a0f748eb8309f36f15ad65e550
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-01-04 17:38:15 +0000

    fix: cache logic

--- y/_db/utils/contract.py
+++ y/_db/utils/contract.py
@@ -6,7 +6,7 @@ from a_sync import a_sync
 from pony.orm import db_session
 from y._db.common import token_attr_threads
 from y._db.entities import retry_locked
-from y._db.utils._ep import _get_get_token
+from y._db.utils._ep import _get_get_block, _get_get_token
 
 
 logger = logging.getLogger(__name__)
@@ -16,14 +16,16 @@ logger = logging.getLogger(__name__)
 @retry_locked
 def get_deploy_block(address: str) -> Optional[int]:
     get_token = _get_get_token()
-    deploy_block = get_token(address, sync=True).deploy_block
-    logger.debug('%s deploy block from cache: %s', address, deploy_block)
-    return deploy_block
+    if deploy_block := get_token(address, sync=True).deploy_block:
+        logger.debug('%s deploy block from cache: %s', address, deploy_block.number)
+        return deploy_block.number
+    logger.debug('%s deploy block not cached, fetching from chain')
 
 @a_sync(default='async', executor=token_attr_threads)
 @db_session
 @retry_locked
 def set_deploy_block(address: str, deploy_block: int) -> None:
+    get_block = _get_get_block()
     get_token = _get_get_token()
-    get_token(address, sync=True).deploy_block = deploy_block
+    get_token(address, sync=True).deploy_block = get_block(deploy_block, sync=True)
     logger.debug('deploy block cached for %s: %s', address, deploy_block)
