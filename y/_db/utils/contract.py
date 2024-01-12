a851d077f8d2bc7b6c48cbcd5ecc08e110086acf
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-01-12 16:47:43 +0000

    fix: eth_portfolio conflict on importing both

--- y/_db/utils/contract.py
+++ y/_db/utils/contract.py
@@ -3,8 +3,8 @@ import logging
 from typing import Optional
 
 from brownie import chain
+from y._db.decorators import a_sync_read_db_session, a_sync_write_db_session
 from y._db.utils._ep import _get_get_token
-from y._db.utils.decorators import a_sync_read_db_session, a_sync_write_db_session
 from y._db.utils.utils import ensure_block
 
 
