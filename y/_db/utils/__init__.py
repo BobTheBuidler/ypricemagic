be39c8aa848a72ee52555251bb6fe124add7200b
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-01-12 16:47:43 +0000

    feat: import ensure_chain util to parent module

--- y/_db/utils/__init__.py
+++ y/_db/utils/__init__.py
@@ -2,7 +2,7 @@
 from y._db import bind_db, generate_mapping
 from y._db.config import connection_settings
 from y._db.entities import db
-from y._db.utils.utils import get_chain, get_block
+from y._db.utils.utils import ensure_chain, get_chain, get_block
 
 bind_db(db, **connection_settings)
 generate_mapping(db)
