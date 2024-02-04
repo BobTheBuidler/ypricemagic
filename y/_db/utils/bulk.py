055e82962a0ca237838ae06d801e91b60290113c
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-02-04 22:20:58 +0000

    feat: make bulk_insert portable

--- y/_db/utils/bulk.py
+++ y/_db/utils/bulk.py
@@ -1,27 +1,27 @@
 
 from typing import Any, Iterable
-from pony.orm import ProgrammingError
+from pony.orm import Database, ProgrammingError
 
 from y import ENVIRONMENT_VARIABLES as ENVS
 from y._db import entities
 from y._db.decorators import a_sync_write_db_session
 
-def execute(sql: str) -> None:
+def execute(sql: str, db: Database = entities.db) -> None:
     try:
-        entities.db.execute(sql)
+        db.execute(sql)
     except ProgrammingError as e:
         raise ValueError(e, sql) from e
 
-def stringify_column_value(value: Any) -> str:
+def stringify_column_value(value: Any, provider: str) -> str:
     if value is None:
         return 'null'
     elif isinstance(value, bytes):
-        if ENVS.DB_PROVIDER == 'postgres':
+        if provider == 'postgres':
             #return f"E'\\x{value.hex()}"
             return f"'{value.decode()}'::bytea"
-        elif ENVS.DB_PROVIDER == 'sqlite':
+        elif provider == 'sqlite':
             return f"X'{value.hex()}'"
-        raise NotImplementedError(ENVS.DB_PROVIDER)
+        raise NotImplementedError(provider)
     elif isinstance(value, str):
         return f"'{value}'"
     elif isinstance(value, int):
@@ -29,15 +29,21 @@ def stringify_column_value(value: Any) -> str:
     else:
         raise NotImplementedError(type(value), value)
         
-def build_row(row: Iterable[Any]) -> str:
-    return f"({','.join(stringify_column_value(col) for col in row)})"
+def build_row(row: Iterable[Any], provider: str) -> str:
+    return f"({','.join(stringify_column_value(col, provider) for col in row)})"
 
 @a_sync_write_db_session
-def insert(entity_type: entities.db.Entity, columns: Iterable[str], items: Iterable[Iterable[Any]]):
-    data = ",".join(build_row(i) for i in items)
-    if ENVS.DB_PROVIDER == 'sqlite':
-        execute(f'insert or ignore into {entity_type.__name__.lower()} ({",".join(columns)}) values {data}')
-    elif ENVS.DB_PROVIDER == 'postgres':
-        execute(f'insert into {entity_type.__name__.lower()} ({",".join(columns)}) values {data} on conflict do nothing')
+def insert(
+    entity_type: entities.db.Entity, 
+    columns: Iterable[str], 
+    items: Iterable[Iterable[Any]],
+    *,
+    db: Database = entities.db,
+) -> None:
+    data = ",".join(build_row(i, db.provider_name) for i in items)
+    if db.provider_name == 'sqlite':
+        execute(f'insert or ignore into {entity_type.__name__.lower()} ({",".join(columns)}) values {data}', db=db)
+    elif db.provider_name == 'postgres':
+        execute(f'insert into {entity_type.__name__.lower()} ({",".join(columns)}) values {data} on conflict do nothing', db=db)
     else:
-        raise NotImplementedError(ENVS.DB_PROVIDER)
+        raise NotImplementedError(db.provider_name)
