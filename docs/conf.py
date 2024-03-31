b72f79526356f5ec387d5074566ec46c18957a2d
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-03-30 23:21:18 -0400

    feat(docs): docs! (#535)

---
+++ docs/conf.py
@@ -0,0 +1,53 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+
+project = 'ypricemagic'
+copyright = '2024, BobTheBuidler'
+author = 'BobTheBuidler'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# Looks for objects in documentation of external libraries
+intersphinx_mapping = {
+    'a_sync': ('https://bobthebuidler.github.io/ez-a-sync', None),
+}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+autodoc_default_options = {
+    'private-members': True,
+    'special-members': '__init__,__call__,__getitem__',
+    'inherited-members': True,
+    'member-order': 'groupwise',
+    # hide private methods that aren't relevant to us here
+    #'exclude-members': '_abc_impl,_fget,_fset,_fdel,_ASyncSingletonMeta__instances,_ASyncSingletonMeta__lock'
+}
+autodoc_typehints = "description"
+# Don't show class signature with the class' name.
+autodoc_class_signature = "separated"
+
+automodule_generate_module_stub = True
+
+sys.path.insert(0, os.path.abspath('./eth_portfolio'))
