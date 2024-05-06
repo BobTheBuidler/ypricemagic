61238d8833086bb6e7ef51b9f0b421caa3948121
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-05-06 00:00:25 -0400

    feat: add usdc to base constants (#630)

--- y/constants.py
+++ y/constants.py
@@ -185,6 +185,7 @@ STABLECOINS = {
     },
     Network.Base: {
         "0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA": "usdbc",
+        "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913": "usdc",
     }
 }.get(chain.id, {})
 
