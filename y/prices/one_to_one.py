ce09ec80879dad7fb8ec3a5b2b869d65440c8a48
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-10-14 01:43:16 -0400

    feat: support sdai-usdm and yfimkusd gauges (#708)

--- y/prices/one_to_one.py
+++ y/prices/one_to_one.py
@@ -14,8 +14,11 @@ MAPPING = {
     Network.Mainnet: {
         "0x4da27a545c0c5B758a6BA100e3a049001de870f5": "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9", # stkaave -> aave
         "0x27D22A7648e955E510a40bDb058333E9190d12D4": "0x0cec1a9154ff802e7934fc916ed7ca50bde6844e", # ppool -> pool
-    }
-}.get(chain.id,{})
+        # TODO: algorithmically get gauges
+        "0xcF5136C67fA8A375BaBbDf13c0307EF994b5681D": "0x425BfB93370F14fF525aDb6EaEAcfE1f4e3b5802", # sdai-usdm-gauge -> sdai-usdm
+        "0x590f7e2b211Fa5Ff7840Dd3c425B543363797701": "0x5756bbdDC03DaB01a3900F01Fb15641C3bfcc457", # YFImkUSD-gauge -> YFImkUSD
+    },
+}.get(chain.id, {})
 
 def is_one_to_one_token(token_address: EthAddress) -> bool:
     return token_address in MAPPING
