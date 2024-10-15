11fa47e2defe72a33076739f7dc87afb2bdfde24
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-10-14 22:27:27 -0400

    feat: document some more stuff (#713)

--- y/classes/common.py
+++ y/classes/common.py
@@ -36,7 +36,15 @@ if TYPE_CHECKING:
 logger = getLogger(__name__)
 
 def hex_to_string(h: HexString) -> str:
-    '''returns a string from a HexString'''
+    """
+    Convert a HexString to a string.
+
+    Args:
+        h: The HexString to convert.
+
+    Returns:
+        The converted string.
+    """
     h = h.hex().rstrip("0")
     if len(h) % 2 != 0:
         h += "0"
@@ -61,6 +69,12 @@ class ContractBase(a_sync.ASyncGenericBase, metaclass=ChecksumASyncSingletonMeta
         super().__init__()
     
     def __str__(self) -> str:
+        """
+        Return the contract address as a string.
+
+        Returns:
+            The contract address as a string.
+        """
         return f'{self.address}'
 
     def __repr__(self) -> str:
@@ -98,6 +112,12 @@ class ContractBase(a_sync.ASyncGenericBase, metaclass=ChecksumASyncSingletonMeta
     @a_sync.aka.cached_property
     @stuck_coro_debugger
     async def build_name(self) -> str:
+        """
+        Get the contract's build name.
+
+        Returns:
+            The contract's build name.
+        """
         return await build_name(self.address, sync=False)
     __build_name__: HiddenMethodDescriptor[Self, str]
 
@@ -113,6 +133,15 @@ class ContractBase(a_sync.ASyncGenericBase, metaclass=ChecksumASyncSingletonMeta
 
 
 class ERC20(ContractBase):
+    """
+    Represents an ERC20 token.
+    """
+
+    address: Address
+    """
+    The contract address of the token.
+    """
+
     def __repr__(self) -> str:
         cls = type(self).__name__
         with suppress(AttributeError):
@@ -129,6 +158,12 @@ class ERC20(ContractBase):
     @a_sync.aka.cached_property
     @stuck_coro_debugger
     async def symbol(self) -> str:
+        """
+        The token's symbol.
+
+        Returns:
+            The token's symbol.
+        """
         if self.address == EEE_ADDRESS:
             return {
                 Network.Mainnet: "ETH",
@@ -148,6 +183,12 @@ class ERC20(ContractBase):
     @a_sync.aka.property
     @stuck_coro_debugger
     async def name(self) -> str:
+        """
+        The token's name.
+
+        Returns:
+            The token's name.
+        """
         if self.address == EEE_ADDRESS:
             return "Ethereum"
         import y._db.utils.token as db
@@ -161,6 +202,12 @@ class ERC20(ContractBase):
     @a_sync.aka.cached_property
     @stuck_coro_debugger
     async def decimals(self) -> int:
+        """
+        The number of decimal places for the token.
+
+        Returns:
+            The number of decimal places for the token.
+        """
         if self.address == EEE_ADDRESS:
             return 18
         import y._db.utils.token as db
@@ -176,6 +223,12 @@ class ERC20(ContractBase):
     @a_sync.aka.cached_property
     @stuck_coro_debugger
     async def scale(self) -> int:
+        """
+        Get the scaling factor for the token.
+
+        Returns:
+            The scaling factor for the token.
+        """
         return 10 ** await self.__decimals__(asynchronous=True)
     
     @a_sync.a_sync # Override the leading underscore so a_sync lib doesn't bypass this fn
@@ -195,10 +248,29 @@ class ERC20(ContractBase):
         return await _erc20.totalSupply(self.address, block=block, sync=False)
 
     async def total_supply_readable(self, block: Optional[Block] = None) -> float:
+        """
+        Get the total supply of the token scaled to a human-readable decimal.
+
+        Args:
+            block (optional): The block number to query.
+
+        Returns:
+            The total supply of the token scaled to a decimal.
+        """
         total_supply, scale = await asyncio.gather(self.total_supply(block=block, sync=False), self.__scale__)
         return total_supply / scale
     
     async def balance_of(self, address: AnyAddressType, block: Optional[Block] = None) -> int:
+        """
+        Query the balance of the token for a given address at a specific block.
+
+        Args:
+            address: The address to query.
+            block (optional): The block number to query. Defaults to latest block.
+
+        Returns:
+            The balance of the token held by `address` at block `block`.
+        """
         return await raw_calls.balanceOf(self.address, address, block=block, sync=False)
     
     async def balance_of_readable(self, address: AnyAddressType, block: Optional[Block] = None) -> float:
@@ -212,6 +284,21 @@ class ERC20(ContractBase):
         skip_cache: bool = ENVS.SKIP_CACHE,
         ignore_pools: Tuple[Pool, ...] = (),
     ) -> Optional[UsdPrice]:
+        """
+        Get the price of the token in USD.
+
+        Args:
+            block (optional): The block number to query. Defaults to latest block.
+            return_None_on_failure: If True, return None instead of raising a :class:`~y.exceptions.yPriceMagicError` on failure.
+            skip_cache: If True, skip using the cache while fetching price data.
+            ignore_pools: An optional tuple of pools to ignore when calculating the price.
+
+        Returns:
+            The price of the token in USD, or None if return_None_on_failure is True and the price cannot be retrieved.
+        
+        Raises:
+            yPriceMagicError: If return_None_on_failure is False and the price cannot be retrieved.
+        """
         from y.prices.magic import get_price
         return await get_price(
             self.address, 
@@ -223,6 +310,15 @@ class ERC20(ContractBase):
         )
         
     async def _symbol(self) -> str:
+        """
+        Get the token's symbol from the contract.
+
+        Returns:
+            The token's symbol.
+
+        Raises:
+            NonStandardERC20: If the symbol cannot be retrieved from the contract.
+        """
         symbol = await probe(self.address, ["symbol()(string)", "SYMBOL()(string)", "getSymbol()(string)"])
         if symbol is None:
             # Sometimes the above will fail if the symbol method returns bytes32, as with MKR. Let's try this.
@@ -235,6 +331,15 @@ class ERC20(ContractBase):
         self.__raise_exception('symbol')
     
     async def _name(self) -> str:
+        """
+        Get the token's name from the contract.
+
+        Returns:
+            The token's name.
+
+        Raises:
+            NonStandardERC20: If the name cannot be retrieved from the contract.
+        """
         name = await probe(self.address, ["name()(string)", "NAME()(string)", "getName()(string)"])
         if name is None:
             # Sometimes the above will fail if the name method returns bytes32, as with MKR. Let's try this.
@@ -247,6 +352,15 @@ class ERC20(ContractBase):
         self.__raise_exception('name')
 
     def __raise_exception(self, fn_name: str):
+        """
+        Raise a NonStandardERC20 exception with a custom error message.
+
+        Args:
+            fn_name: The name of the function that failed to retrieve data.
+
+        Raises:
+            NonStandardERC20: Always raised with a custom error message.
+        """
         raise NonStandardERC20(f'''
             Unable to fetch `{fn_name}` for {self.address} on {Network.printable()}
             If the contract is verified, please check to see if it has a strangely named
@@ -262,11 +376,20 @@ class ERC20(ContractBase):
 
 @final
 class WeiBalance(a_sync.ASyncGenericBase):
+    """
+    Represents a balance of a token in wei.
+
+    This class provides utility methods for working with token balances in wei,
+    including converting to readable format, getting the price in USD, and performing
+    arithmetic operations.
+    """
+
     # defaults are stored as class vars to keep instance dicts smaller
     block: Optional[Block] = None
     asynchronous: bool = False
     _skip_cache: bool = ENVS.SKIP_CACHE
     _ignore_pools: Tuple[Pool, ...] = ()
+
     def __init__(
         self, 
         balance: int,
@@ -277,7 +400,17 @@ class WeiBalance(a_sync.ASyncGenericBase):
         ignore_pools: Tuple[Pool, ...] = (),
         asynchronous: bool = False,
         ) -> None:
+        """
+        Initialize a WeiBalance object.
 
+        Args:
+            balance: The balance in wei.
+            token: The token address.
+            block: The block number for the balance (optional).
+            skip_cache: If True, skip using the local cache for price data.
+            ignore_pools: A tuple of pools to ignore when calculating the price.
+            asynchronous: True if this object will be used with its asynchronous API, False for its sync API.
+        """
         if asynchronous != self.asynchronous:
             self.asynchronous = asynchronous
         self.balance = Decimal(balance)
@@ -291,6 +424,12 @@ class WeiBalance(a_sync.ASyncGenericBase):
             self._ignore_pools = ignore_pools
 
     def __str__(self) -> str:
+        """
+        Return the balance in wei as a string.
+
+        Returns:
+            The balance in wei as a string.
+        """
         return str(self.balance)
     
     def __repr__(self) -> str:
@@ -300,9 +439,24 @@ class WeiBalance(a_sync.ASyncGenericBase):
         return hash((self.balance, self.token, self.block, self._skip_cache, self._ignore_pools))
 
     def __bool__(self) -> bool:
+        """
+        Check if the balance is non-zero.
+
+        Returns:
+            True if the balance is non-zero, False otherwise.
+        """
         return bool(self.balance)
 
     def __eq__(self, __o: object) -> bool:
+        """
+        Check if two WeiBalance objects are equal.
+
+        Args:
+            __o: The object to compare with.
+
+        Returns:
+            True if the objects are equal, False otherwise.
+        """
         if isinstance(__o, int):
             return __o == self.balance
         elif isinstance(__o, WeiBalance):
@@ -325,6 +479,18 @@ class WeiBalance(a_sync.ASyncGenericBase):
         raise TypeError(f"'<' not supported between instances of '{self.__class__.__name__}' and '{__o.__class__.__name__}'") from None
 
     def __ge__(self, __o: object) -> bool:
+        """
+        Check if one WeiBalance object is greater than or equal to another.
+
+        Args:
+            __o: The object to compare with.
+
+        Returns:
+            True if the current object is greater than or equal to the other object, False otherwise.
+
+        Raises:
+            TypeError: If the objects are not of the same type.
+        """
         if __o < self:
             return True
         elif type(__o) is type(self):
@@ -376,17 +542,47 @@ class WeiBalance(a_sync.ASyncGenericBase):
             raise TypeError(f"subtraction not supported between instances of '{type(self).__name__}' and '{type(__o).__name__}'") from None
     
     def __mul__(self, __o: Union[int, float, Decimal]) -> "WeiBalance":
+        """
+        Multiply a WeiBalance object by a scalar value.
+
+        Args:
+            __o: The scalar value to multiply by.
+
+        Returns:
+            A new WeiBalance object representing the product.
+
+        Raises:
+            TypeError: If the scalar value is not a number.
+        """
         if not isinstance(__o, (int, float, Decimal)):
             raise TypeError(f"multiplication not supported between instances of '{type(self).__name__}' and '{type(__o).__name__}'") from None
         return WeiBalance(self.balance * Decimal(__o), self.token, self.block, skip_cache=self._skip_cache, ignore_pools=self._ignore_pools)
     
     def __truediv__(self, __o: Union[int, float, Decimal]) -> "WeiBalance":
+        """
+        Divide a WeiBalance object by a scalar value.
+
+        Args:
+            __o: The scalar value to divide by.
+
+        Returns:
+            A new WeiBalance object representing the quotient.
+
+        Raises:
+            TypeError: If the scalar value is not a number.
+        """
         if not isinstance(__o, (int, float, Decimal)):
             raise TypeError(f"division not supported between instances of '{type(self).__name__}' and '{type(__o).__name__}'") from None
         return WeiBalance(self.balance / Decimal(__o), self.token, self.block, skip_cache=self._skip_cache, ignore_pools=self._ignore_pools)
     
     @a_sync.aka.property
     async def readable(self) -> Decimal:
+        """
+        Get the balance scaled to a human-readable decimal.
+
+        Returns:
+            The balance scaled to a decimal.
+        """
         if self.balance == 0:
             return 0
         scale = await self.token.__scale__
@@ -397,6 +593,12 @@ class WeiBalance(a_sync.ASyncGenericBase):
     
     @a_sync.aka.property
     async def price(self) -> Decimal:
+        """
+        Get the price of the token in USD.
+
+        Returns:
+            The price of the token in USD.
+        """
         price = Decimal(await self.token.price(block=self.block, skip_cache=self._skip_cache, ignore_pools=self._ignore_pools, sync=False))
         self._logger.debug("balance: %s  price: %s", self, price)
         return price
@@ -404,6 +606,12 @@ class WeiBalance(a_sync.ASyncGenericBase):
     
     @a_sync.aka.property
     async def value_usd(self) -> Decimal:
+        """
+        Get the value of the balance in USD.
+
+        Returns:
+            The value of the balance in USD.
+        """
         if self.balance == 0:
             return 0
         balance, price = await asyncio.gather(self.__readable__, self.__price__)
@@ -414,6 +622,12 @@ class WeiBalance(a_sync.ASyncGenericBase):
     
     @cached_property
     def _logger(self) -> logging.logging.Logger:
+        """
+        Get the logger for the WeiBalance object.
+
+        Returns:
+            The logger for the WeiBalance object.
+        """
         return logging.get_price_logger(self.token.address, self.block, extra=self.__class__.__name__)
 
 
@@ -426,9 +640,16 @@ class _Loader(ContractBase):
         self._loaded = None
         self.__exc = None
         self.__task = None
+
     def __await__(self) -> Generator[Any, None, Literal[True]]:
-        """Returns `True` once the `_Loader` has loaded all relevant data thru the current block"""
+        """
+        Returns `True` once the `_Loader` has loaded all relevant data thru the current block.
+
+        Returns:
+            A generator that yields `True` once the `_Loader` has loaded all relevant data thru the current block.
+        """
         return self.loaded.__await__()
+
     @abc.abstractmethod
     async def _load(self) -> NoReturn:
         """
@@ -437,14 +658,28 @@ class _Loader(ContractBase):
         """
     @property
     def loaded(self) -> Awaitable[Literal[True]]:
-        """Returns `True` once the `_Loader` has loaded all relevant data thru the current block"""
+        """
+        Returns `True` once the `_Loader` has loaded all relevant data thru the current block.
+
+        Returns:
+            An awaitable that resolves to `True` once the `_Loader` has loaded all relevant data thru the current block.
+        """
         self._task  # ensure task is running and not errd
         if self._loaded is None:
             self._loaded = a_sync.Event(name=self)
         return self._loaded.wait()
+
     @property
     def _task(self) -> "asyncio.Task[NoReturn]":
-        """The task that runs `self._load() for this `_Loader`"""
+        """
+        The task that runs `self._load()` for this `_Loader`.
+
+        Returns:
+            The task that runs `self._load()` for this `_Loader`.
+
+        Raises:
+            Exception: If the _Loader has any exception, it is raised.
+        """
         if self.__exc:
             # create a new duplicate exc instead of building a massive traceback on the original
             raise type(self.__exc)(*self.__exc.args).with_traceback(self.__tb)
@@ -453,14 +688,23 @@ class _Loader(ContractBase):
             self.__task = asyncio.create_task(coro=self.__load(), name=f"{self}.__load()")
             self.__task.add_done_callback(self._done_callback)
         return self.__task
+
     def _done_callback(self, task: "asyncio.Task[Any]") -> None:
-        """called on `self._task` when it completes, if applicable"""
+        """
+        Called on `self._task` when it completes, if applicable.
+
+        Args:
+            task: The completed task.
+        """
         if e := task.exception():
             logger.error("exception while loading %s: %s", self, e)
             logger.exception(e)
             self.__task = None
+
     async def __load(self) -> NoReturn:
-        """loads the loader and catches any exceptions"""
+        """
+        Loads the loader and catches any exceptions.
+        """
         try:
             await self._load()
         except Exception as e:
@@ -473,17 +717,34 @@ class _Loader(ContractBase):
 
 
 class _EventsLoader(_Loader):
-    """Used for use cases where you need to load event data thru present time before proceeding, and then continue loading data in the background."""
+    """
+    Used for use cases where you need to load event data thru present time before proceeding,
+    and then continue loading data in the background.
+    """
+
     @abc.abstractproperty
     def _events(self) -> "Events":
-        ...
+        """
+        The Events object associated with this _EventsLoader.
+        """
+
     @property
     def loaded(self) -> Awaitable[Literal[True]]:
+        """
+        Returns `True` once the `_Loader` has loaded all relevant data thru the current block.
+
+        Returns:
+            An awaitable that resolves to `True` once the `_Loader` has loaded all relevant data thru the current block.
+        """
         self._task  # ensure task is running and not err'd
         if self._loaded is None:
             self._loaded = asyncio.ensure_future(self._events._lock.wait_for(self._init_block))
         return self._loaded
+
     async def _load(self) -> NoReturn:
+        """
+        Load the event data in the background.
+        """
         # TODO: extend this for constant loading
         async for _ in self._events.events(self._init_block):
             pass
