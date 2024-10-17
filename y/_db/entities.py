a502e384180128e4414988575b248be9a2a66e4a
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-10-17 21:41:05 +0000

    fix: size must be posarg

--- y/_db/entities.py
+++ y/_db/entities.py
@@ -53,7 +53,7 @@ class Block(db.Entity, _AsyncEntityMixin):
 
 class Address(db.Entity, _AsyncEntityMixin):
     chain = Required(Chain, lazy=True, reverse="addresses")
-    address = Required(str, size=42, lazy=True)
+    address = Required(str, 42, lazy=True)
     PrimaryKey(chain, address)
     notes = Optional(str, lazy=True)
     
@@ -87,7 +87,7 @@ class TraceCacheInfo(db.Entity):
 
 class LogCacheInfo(db.Entity):
     chain = Required(Chain, index=True)
-    address = Required(str, size=42, index=True)
+    address = Required(str, 42, index=True)
     topics = Required(bytes)
     PrimaryKey(chain, address, topics)
     cached_from = Required(int)
@@ -95,8 +95,8 @@ class LogCacheInfo(db.Entity):
 
 class LogTopic(db.Entity):
     "Just makes the :ref:`Log` db smaller."
-    dbid = PrimaryKey(int, auto=True, size=64)
-    topic = Required(str, size=64, unique=True, lazy=True)
+    dbid = PrimaryKey(int, 64, auto=True)
+    topic = Required(str, 64, unique=True, lazy=True)
 
     logs_as_topic0 = Set("Log", reverse="topic0")
     logs_as_topic1 = Set("Log", reverse="topic1")
@@ -105,8 +105,8 @@ class LogTopic(db.Entity):
 
 class Hashes(db.Entity):
     "Just makes :ref:`Log` pk and indexes smaller."
-    dbid = PrimaryKey(int, auto=True, size=64)
-    hash = Required(str, size=64, unique=True)
+    dbid = PrimaryKey(int, 64, auto=True)
+    hash = Required(str, 64, unique=True)
 
     logs_for_tx = Set("Log", reverse="tx")
     logs_for_addr = Set("Log", reverse="address")
@@ -114,7 +114,7 @@ class Hashes(db.Entity):
 class Log(db.Entity):
     block = Required(Block, index=True, lazy=True)
     tx = Required(Hashes, lazy=True)
-    log_index = Required(int, size=16, lazy=True)
+    log_index = Required(int, 16, lazy=True)
     PrimaryKey(block, tx, log_index)
 
     address = Required(Hashes, index=True, lazy=True)
@@ -138,9 +138,9 @@ class Log(db.Entity):
 class Trace(db.Entity):
     id = PrimaryKey(int, auto=True)
     block = Required(Block, index=True, lazy=True)
-    hash = Required(str, size=64, index=True, lazy=True)
-    from_address = Required(str, size=42, index=True, lazy=True)
-    to_address = Required(str, size=42, index=True, lazy=True)
+    hash = Required(str, 64, index=True, lazy=True)
+    from_address = Required(str, 42, index=True, lazy=True)
+    to_address = Required(str, 42, index=True, lazy=True)
     raw = Required(bytes)
 
 class BlockAtTimestamp(db.Entity):
