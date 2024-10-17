5ba1bf727a2118e8e81defe28e95c3c49397b1ab
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-10-17 07:00:09 +0000

    fix: decode HexBytes32

--- y/_db/utils/logs.py
+++ y/_db/utils/logs.py
@@ -280,7 +280,7 @@ def _decode_hook(typ, obj):
             return uint(obj)
         elif isinstance(obj, str):
             return uint(obj, 16)
-    elif typ is HexBytes:
+    elif typ in [HexBytes, HexBytes32]:
         return HexBytes(obj)
     elif typ is Address:
         return checksum(obj)
