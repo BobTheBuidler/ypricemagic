a489d467f6f283b6a0231a18d2870f4ce421a64f
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-11-25 01:10:25 -0400

    Update convert.py

--- y/convert.py
+++ y/convert.py
@@ -1,6 +1,11 @@
 from functools import lru_cache
 
+import brownie.convert
+import checksum_dict._key
+import eth_event.main
+import eth_utils
 from brownie.convert.datatypes import EthAddress, HexBytes
+from checksum_dict import to_checksum_address
 from eth_typing import ChecksumAddress, HexAddress
 
 from y import ENVIRONMENT_VARIABLES as ENVS
@@ -18,3 +23,26 @@ def _int_to_address(int_address: int) -> HexAddress:
     hex_value = HexBytes(int_address).hex()[2:]
     padding = "0" * (40 - len(hex_value))
     return f"0x{padding}{hex_value}"
+
+def _monkey_patch_dependencies():
+    """
+    Monkey patch dependency checksums with faster versions.
+    """
+    # this monkey patches checksum_dict's checksumming with our lru_cache
+    checksum_dict._key.to_checksum_address = to_address
+    
+    # this monkey patches eth_event's address checksumming with our lru_cache
+    eth_event.main.to_checksum_address = to_address
+    
+    # this monkey patches brownie's convert.to_address with our lru_cache
+    brownie.convert.to_address = to_address
+    
+    # y.convert.to_address depends on brownie.convert.to_address which depends on eth_utils.to_checksum_address
+    # so we cannot overwrite eth_utils.to_checksum_address with y.convert.to_address like we do for eth_event
+    # or we will create a circular dependency
+    
+    # this monkey patches brownie's EthAddress class with faster execution
+    eth_utils.to_checksum_address = to_checksum_address
+    
+    # this monkey patches something else I don't remember now with faster execution
+    eth_utils.address.to_checksum_addres = to_checksum_address
