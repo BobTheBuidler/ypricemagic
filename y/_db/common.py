979b28aaa319b8019ac1de2a1c4c8b0a5f8e8eb4
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-11-28 02:28:52 -0400

    fix: set checkpoint

--- y/_db/common.py
+++ y/_db/common.py
@@ -250,13 +250,10 @@ class _DiskCachedMixin(a_sync.ASyncIterable[T], Generic[T, C], metaclass=abc.ABC
         """
         if objs:
             self._objects.extend(objs)
-            if (
-                self._is_reusable
-                and len(self._objects) > tuple(self._checkpoints.values())[-1] + 50
-            ):
+            if self._is_reusable:
                 block = self._get_block_for_obj(self._objects[-1])
                 self._checkpoints[block] = len(self._objects)
-
+                
     def _remove(self, obj: T) -> None:
         self._objects.remove(obj)
         self._pruned += 1
