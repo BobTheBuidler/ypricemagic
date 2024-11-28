4cafe295f508146565bed33ff37d112921624233
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-11-28 03:45:54 -0400

    feat: use block checkpoints to make Filter classes faster (#812)
    * feat: use block checkpoints to make Filter classes faster
    
    * chore: `black .`
    
    * fix: set checkpoint
    
    * chore: `black .`
    
    * fix: yield count
    
    * fix: write checkpoints when loading from db
    
    * chore: `black .`
    
    * fix: attribute error
    
    * fix: attribute error
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- y/_db/common.py
+++ y/_db/common.py
@@ -249,7 +249,10 @@ class _DiskCachedMixin(a_sync.ASyncIterable[T], Generic[T, C], metaclass=abc.ABC
             objs: The objects to extend the list with.
         """
         if objs:
-            return self._objects.extend(objs)
+            self._objects.extend(objs)
+            if self.is_reusable:
+                block = self._get_block_for_obj(self._objects[-1])
+                self._checkpoints[block] = len(self._objects)
 
     def _remove(self, obj: T) -> None:
         self._objects.remove(obj)
@@ -275,6 +278,28 @@ class _DiskCachedMixin(a_sync.ASyncIterable[T], Generic[T, C], metaclass=abc.ABC
             await self._extend(
                 await self.executor.run(self.cache.select, from_block, cached_thru)
             )
+            if self.is_reusable:
+                objs_per_chunk = 50
+                num_checkpoints = len(self._objects) // objs_per_chunk
+                checkpoint_indexes = (
+                    i * objs_per_chunk for i in range(1, num_checkpoints)
+                )
+                for index in checkpoint_indexes:
+                    obj = self._objects[index]
+                    if index < len(self._objects):
+                        next_obj = self._objects[index + 1]
+                        while self._get_block_for_obj(obj) == self._get_block_for_obj(
+                            next_obj
+                        ):
+                            obj = next_obj
+                            index += 1
+                            try:
+                                next_obj = self._objects[index + 1]
+                            except IndexError:
+                                break
+
+                    self._checkpoints[self._get_block_for_obj(obj)] = index
+
             logger.info(
                 "%s loaded %s objects thru block %s from disk",
                 self,
@@ -303,6 +328,7 @@ class Filter(_DiskCachedMixin[T, C]):
     __slots__ = (
         "from_block",
         "to_block",
+        "_checkpoints",
         "_interval",
         "_lock",
         "__dict__",
@@ -333,6 +359,7 @@ class Filter(_DiskCachedMixin[T, C]):
             self._sleep_time = sleep_time
         if verbose != self._verbose:
             self._verbose = verbose
+        self._checkpoints = {}
         super().__init__(executor=executor, is_reusable=is_reusable)
 
     def __aiter__(self) -> AsyncIterator[T]:
@@ -373,6 +400,25 @@ class Filter(_DiskCachedMixin[T, C]):
         debug_logs = logger.isEnabledFor(logging.DEBUG)
         yielded = 0
         done_thru = 0
+        if self.is_reusable and self._objects:
+            if block is None:
+                for obj in self._objects:
+                    yield obj
+                    yielded += 1
+            else:
+                for _block in self._checkpoints:
+                    if _block <= block:
+                        checkpoint_block = _block
+                    else:
+                        break
+
+                checkpoint_index = self._checkpoints[checkpoint_block]
+                for obj in self._objects[:checkpoint_index]:
+                    yield obj
+                    yielded += 1
+
+            done_thru = self._get_block_for_obj(obj)
+
         while True:
             if block is None or done_thru < block:
                 if self.is_asleep:
