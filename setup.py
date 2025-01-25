7227c6a94f61a61b53087685f8ce47f0fddfd392
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-01-25 18:48:38 -0400

    fix: exclude tests from distro

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r") as f:
 
 setup(
     name="ypricemagic",
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests", "tests.*"]),
     use_scm_version={
         "root": ".",
         "relative_to": __file__,
