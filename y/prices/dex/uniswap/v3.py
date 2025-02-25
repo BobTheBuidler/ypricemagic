f0a67cfaa4db89d239a188fe47f4b71abf038cea
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-02-25 21:10:12 +0000

    feat: cache PriceError in local db

--- y/prices/dex/uniswap/v3.py
+++ y/prices/dex/uniswap/v3.py
@@ -681,7 +681,8 @@ class UniV3Pools(ProcessedEvents[UniswapV3Pool]):
         """
         return obj._deploy_block
 
-# TODO move this to its own file
+
+# TODO: move this into velodrome module, might need to define a separate class for the router
 class SlipstreamPools(UniV3Pools):
     def _process_event(self, event: _EventItem) -> UniswapV3Pool:
         token0, token1, tick_spacing, pool = event.values()
