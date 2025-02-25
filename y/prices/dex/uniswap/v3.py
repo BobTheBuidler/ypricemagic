c438b5fcbc4ca490b69c910607cb8f9f61fc6a5c
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-02-25 21:10:12 +0000

    fix: missing comma

--- y/prices/dex/uniswap/v3.py
+++ y/prices/dex/uniswap/v3.py
@@ -73,7 +73,7 @@ forked_deployments = {
             "quoter": "0x89D8218ed5fF1e46d8dcd33fb0bbeE3be1621466",
             "fee_tiers": [3000, 500, 10_000, 100],
         }
-    ]
+    ],
     Network.Base: [
         {
             # Aerodrome slipstream
