ed660e58c26101d5c3d15ed2bfddfc5a2f75560e
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-03-29 06:20:58 +0000

    chore: cleanup contracts module

--- y/prices/dex/balancer/v2.py
+++ y/prices/dex/balancer/v2.py
@@ -284,7 +284,8 @@ class BalancerEvents(ProcessedEvents[Tuple[HexBytes, EthAddress, Block]]):
         self.asynchronous = asynchronous
         self.__tasks = []
 
-    def _include_event(self, event: _EventItem) -> Awaitable[bool]:
+    @staticmethod
+    def _include_event(event: _EventItem) -> Awaitable[bool]:
         """
         Determine whether to include a specific event.
 
@@ -302,7 +303,7 @@ class BalancerEvents(ProcessedEvents[Tuple[HexBytes, EthAddress, Block]]):
         # NOTE: For some reason the Balancer fork on Fantom lists "0x3e522051A9B1958Aa1e828AC24Afba4a551DF37d"
         #       as a pool, but it is not a contract. This handler will prevent it and future cases from causing problems.
         # NOTE: this isn't really optimized as it still runs semi-synchronously but its better than what was had previously
-        return self.executor.run(contracts.is_contract, event["poolAddress"])
+        return BalancerEvents.executor.run(contracts.is_contract, event["poolAddress"])
 
     def _process_event(self, event: _EventItem) -> "BalancerV2Pool":
         """
