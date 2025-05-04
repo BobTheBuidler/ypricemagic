2f1c0c6f7eb58a4c9c059a0c99b9f1db79989bf8
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-05-04 17:12:27 -0400

    Update common.py

--- y/_db/common.py
+++ y/_db/common.py
@@ -632,7 +632,7 @@ class Filter(_DiskCachedMixin[T, C]):
                 await self._lock.wait_for(done_thru + 1)
             if self._exc is not None:
                 # raise a copy of it so multiple waiters don't destroy the traceback
-                raise deepcopy(self._exc).with_traceback(self._exc.__traceback__)
+                raise deepcopy(self._exc)
             if to_yield := self._objects[yielded - self._pruned :]:
                 if from_block and not reached_from_block:
                     objs = skip_too_early(to_yield)
@@ -931,7 +931,8 @@ class Filter(_DiskCachedMixin[T, C]):
             # garbage collected so there is no need to log the "destroy pending task" message.
             self._task._log_destroy_pending = False
         if self._task.done() and (e := self._task.exception()):
-            raise e.with_traceback(e.__traceback__)
+            # copy the exc so the traceback doesn't get destroyed by other waiters
+            raise deepcopy(e)
 
     async def __insert_chunk(
         self,
