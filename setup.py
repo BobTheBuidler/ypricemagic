dd5414daf06de6f360c5c41d5d601419838568a5
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-05-05 20:26:39 -0400

    fix: build errs on higher pythons

--- setup.py
+++ setup.py
@@ -47,6 +47,8 @@ try:
             "--disable-error-code=assignment",
             "--disable-error-code=var-annotated",
             "--disable-error-code=arg-type",
+            "--disable-error-code=typeddict-item",
+            "--disable-error-code=type-var",
         ]
     )
 except ImportError:
