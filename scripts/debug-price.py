623a18bb6ca6de4eb7ea159479ed65aca77fbda5
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-06-18 16:53:52 +0000

    fix: missing cli price debugger script

--- scripts/debug-price.py
+++ scripts/debug-price.py
@@ -1,20 +1,5 @@
 import os
-from logging import DEBUG, INFO, StreamHandler, getLogger
-
-from brownie import chain
-
-import y
-from y.prices.stable_swap.curve import startup_logger
-
-y_logger = getLogger("y")
-y_logger.setLevel(DEBUG)
-y_logger.addHandler(StreamHandler())
-startup_logger.setLevel(INFO)
-getLogger("y._db.common").setLevel(INFO)
-getLogger("y._db.utils.bulk").setLevel(INFO)
-getLogger("y._db.utils.logs").setLevel(INFO)
-getLogger("y.classes.common").setLevel(INFO)
-
+from y.prices.utils.debug import debug_price
 
 def main():
     """
@@ -25,35 +10,15 @@ def main():
 
     Environment Variables:
         BAD: The address of the token to debug. This variable must be set.
-             If not set, a ValueError will be raised.
         BLOCK: The block number at which to retrieve the token price. If not specified,
                the current block height is used.
 
     Raises:
         ValueError: If the BAD environment variable is not set.
-
-    Examples:
-        To debug the price of a token with address '0xTokenAddress' at block 1234567:
-        ```
-        export BAD=0xTokenAddress
-        export BLOCK=1234567
-        python debug-price.py
-        ```
-
-        To debug the price of a token with address '0xTokenAddress' at the current block:
-        ```
-        export BAD=0xTokenAddress
-        python debug-price.py
-        ```
-
-    See Also:
-        - :func:`y.get_price`: For more details on how the price is retrieved.
     """
     BAD = os.environ.get("BAD")
     BLOCK = os.environ.get("BLOCK")
     if not BAD:
         raise ValueError("You must specify a token to debug by setting `BAD` env var")
-    if not BLOCK:
-        BLOCK = chain.height
-        y_logger.warning("no `BLOCK` specified, using %s", BLOCK)
-    y.get_price(BAD, int(BLOCK), skip_cache=True)
+    block = int(BLOCK) if BLOCK else None
+    debug_price(BAD, block)
