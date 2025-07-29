4987f2661ac33b806ce683f97dc542b44acd727c
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-07-29 13:47:12 -0400

    Update curve.py

--- y/prices/stable_swap/curve.py
+++ y/prices/stable_swap/curve.py
@@ -14,6 +14,7 @@ from brownie import ZERO_ADDRESS
 from brownie.convert.datatypes import EthAddress
 from brownie.exceptions import ContractNotFound, EventLookupError
 from brownie.network.event import _EventItem
+from eth_abi.exceptions import InvalidPointer
 from typing_extensions import Self
 from web3.exceptions import ContractLogicError
 
