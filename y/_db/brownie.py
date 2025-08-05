21abcf4555b2a2f7ce28a6507685b40555c3f925
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-08-05 22:27:25 +0000

    fix typing

--- y/_db/brownie.py
+++ y/_db/brownie.py
@@ -99,9 +99,10 @@ class AsyncCursor:
         if self._db is not None:
             raise RuntimeError("already connected")
         async with sqlite_lock:
-            if self._db is None:
-                self._db = await aiosqlite.connect(self._filename, isolation_level=None)
-                self._execute = self._db.execute
+            if self._db is not None:
+                return
+            self._db = await aiosqlite.connect(self._filename, isolation_level=None)
+            self._execute = self._db.execute
 
     async def insert(self, table: str, *values: Any) -> None:
         raise NotImplementedError
