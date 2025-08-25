c2c41250cde2a6182addeb9cc68ef315653a5b82
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-08-25 12:48:24 -0400

    chore: exempt DXD from sense check on mainnet (#1132)
    * chore: exempt DXD from sense check on mainnet
    
    * chore: compile C files for source control
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- build/__native_internal_839fc216801717d47e67.h
+++ build/__native_internal_839fc216801717d47e67.h
@@ -6,7 +6,7 @@
 
 int CPyGlobalsInit(void);
 
-extern PyObject *CPyStatics[650];
+extern PyObject *CPyStatics[651];
 extern const char * const CPyLit_Str[];
 extern const char * const CPyLit_Bytes[];
 extern const char * const CPyLit_Int[];
