f2c12988949e9ff5c1d6cd37b2d182cfa72000bd
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-09-04 23:21:26 -0400

    use alru_cache

--- y/_db/asyncpg_pool.py
+++ y/_db/asyncpg_pool.py
@@ -1,27 +1,20 @@
 import asyncpg
 import os
 
-_asyncpg_pool = None
+from async_lru import alru_cache
 
 
+@alru_cache(maxsize=None)
 async def get_asyncpg_pool():
     """
     Get or create a global asyncpg connection pool using environment/config variables.
     """
-    global _asyncpg_pool
-    if _asyncpg_pool is None:
-        db_host = os.environ.get("DB_HOST", "localhost")
-        db_port = int(os.environ.get("DB_PORT", 5432))
-        db_user = os.environ.get("DB_USER", "postgres")
-        db_password = os.environ.get("DB_PASSWORD", "")
-        db_database = os.environ.get("DB_DATABASE", "ypricemagic")
-        _asyncpg_pool = await asyncpg.create_pool(
-            host=db_host,
-            port=db_port,
-            user=db_user,
-            password=db_password,
-            database=db_database,
-            min_size=1,
-            max_size=10,
-        )
-    return _asyncpg_pool
+    return await asyncpg.create_pool(
+        host=os.environ.get("DB_HOST", "localhost"),
+        port=int(os.environ.get("DB_PORT", 5432)),
+        user=os.environ.get("DB_USER", "postgres"),
+        password=os.environ.get("DB_PASSWORD", ""),
+        database=os.environ.get("DB_DATABASE", "ypricemagic"),
+        min_size=1,
+        max_size=10,
+    )
