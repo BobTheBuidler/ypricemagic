71bc7b4b2d6a7237da5b641eca7a00da48408dce
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-12-08 23:31:34 -0400

    fix(mypy): fix mypy errs

--- y/contracts.py
+++ y/contracts.py
@@ -1036,7 +1036,7 @@ def _resolve_proxy(address) -> Tuple[str, List]:
     # Just leave this code where it is for a helpful debugger as needed.
     if address == "":
         raise Exception(
-            f"""implementation: {implementation}
+            f"""implementation: {implementation!r}
             implementation_eip1967: {len(implementation_eip1967)} {implementation_eip1967!r}
             implementation_eip1822: {len(implementation_eip1822)} {implementation_eip1822!r}"""
         )
@@ -1238,9 +1238,9 @@ async def _resolve_proxy_async(address) -> Tuple[str, List]:
     # Just leave this code where it is for a helpful debugger as needed.
     if address == "":
         raise Exception(
-            f"""implementation: {implementation}
-            implementation_eip1967: {len(implementation_eip1967)} {implementation_eip1967}
-            implementation_eip1822: {len(implementation_eip1822)} {implementation_eip1822}"""
+            f"""implementation: {implementation!r}
+            implementation_eip1967: {len(implementation_eip1967)} {implementation_eip1967!r}
+            implementation_eip1822: {len(implementation_eip1822)} {implementation_eip1822!r}"""
         )
 
     if len(implementation_eip1967) > 0 and int(implementation_eip1967.hex(), 16):
